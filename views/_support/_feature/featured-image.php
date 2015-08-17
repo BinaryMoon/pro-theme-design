d1347d81e799374eebef0aaccd7b0bb144546e0f
BinaryMoon <binarymoon@gmail.com>
2015-08-17 14:35:00 +0100

    first draft of new documentation for featured image

---
+++ views/_support/_feature/featured-image.php
@@ -0,0 +1,14 @@
+<p class="intro">Featured images are the images displayed as thumbnails in various places around the theme. All Pro Theme Design themes support Featured Images.</p>
+
+<p>To specify the featured image used you should:</p>
+
+<ol>
+    <li>Edit the post you want to add the featured image to.</li>
+    <li>Locate the Featured Image module in the sidebar</li>
+    <li>Click the on Set featured image button</li>
+    <li>Select or Upload an image using the options provided</li>
+    <li>Once selected, press Set featured image</li>
+    <li>Finally, click the Update button to save changes to your post</li>
+</ol>
+
+<p>If there is no Featured Image set then your theme will look for any other image uploaded to the post in question. If there is one, it will be used like a Featured Image.</p>
