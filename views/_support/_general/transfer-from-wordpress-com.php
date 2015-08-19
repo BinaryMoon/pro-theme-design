0fdf4473b0e94c109d6101e6161c0a260008e123
BinaryMoon <binarymoon@gmail.com>
2015-08-19 14:20:21 +0100

    update transfer from wordpress.com copy

--- views/_support/_general/transfer-from-wordpress-com.php
+++ views/_support/_general/transfer-from-wordpress-com.php
@@ -1,5 +1,5 @@
-<p class="intro">Unfortunately themes are not downloadable from WordPress.com. This is a standard part of the WordPress.com policy that is mentioned on the <a href="https://en.support.wordpress.com/premium-themes/">WordPress.com premium theme information page</a>.</p>
+<p class="intro">Themes are not downloadable from WordPress.com - if you wish to use a theme on a self hosted website then you should buy one of our themes from <a href="https://creativemarket.com/BinaryMoon?u=BinaryMoon">Creative Market</a> or <a href="<?php echo path( 'theme-club/' ); ?>">join the theme club</a>.</p>
 
-<p>To download one of our themes you either have to be a <a href="<?php echo path( 'theme-club/' ); ?>">member of our theme club</a>, or the theme has to be purchased from the <a href="https://creativemarket.com/BinaryMoon?u=BinaryMoon">Pro Theme Design Store on Creative Market</a>.</p>
+<p>If you've already purchased the theme from wordpress.com then you can't transfer it. This is a standard part of the WordPress.com policy that is mentioned on the <a href="https://en.support.wordpress.com/premium-themes/">WordPress.com premium theme information page</a>.</p>
 
 <p>If you have purchased your theme in the last 30 days on wordpress.com then you can <a href=" http://en.support.wordpress.com/my-upgrades/refunds/">find out how to get a refund here</a>.</p>
