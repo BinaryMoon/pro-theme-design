d68b87d5608e38ca89657082ba1e7217477acf53
BinaryMoon <binarymoon@gmail.com>
2015-08-24 14:46:43 +0100

    make .org themes display first

--- views/home.php
+++ views/home.php
@@ -16,16 +16,16 @@
             </div>
 
             <nav class="tabs">
-                <a href="http://theme.wordpress.com/themes/by/pro-theme-design/" class="selected" data-target="wordpress">
-                    <strong>WordPress.com</strong> <small>WordPress in the Cloud</small>
-                </a>
-                <a href="https://creativemarket.com/BinaryMoon?u=BinaryMoon" data-target="creative-market">
+                <a href="https://creativemarket.com/BinaryMoon?u=BinaryMoon" class="selected" data-target="creative-market">
                     <strong>WordPress.org</strong> <small>Self-Hosted Websites</small>
+                </a>
+                <a href="http://theme.wordpress.com/themes/by/pro-theme-design/" data-target="wordpress">
+                    <strong>WordPress.com</strong> <small>WordPress in the Cloud</small>
                 </a>
             </nav>
         </header>
 
-        <div class="promo wordpress" id="theme-wrapper">
+        <div class="promo creative-market" id="theme-wrapper">
 <?php
 
     $count_wordpress = 0;
@@ -61,15 +61,15 @@
         }
 
 ?>
-            <div class="<?php echo implode( ' ', $class ); ?>" data-wp-price="<?php echo $theme['price-wpcom']; ?>" data-cm-price="<?php echo $theme['price-cm']; ?>" data-wp-url="https://wordpress.com/themes/<?php echo $theme['url-wpcom']; ?>/" data-cm-url="<?php echo $theme['url-wporg']; ?>">
+            <div class="<?php echo implode( ' ', $class ); ?>" data-wp-price="<?php echo $theme['price-wpcom']; ?>" data-cm-price="<?php echo $theme['price-cm']; ?>" data-wp-url="<?php echo $theme['url-wpcom']; ?>" data-cm-url="<?php echo $theme['url-wporg']; ?>">
                 <div class="large-8 columns image">
-                    <a href="https://wordpress.com/themes/<?php echo $theme['url-wpcom']; ?>/" target="_blank">
+                    <a href="https://wordpress.com/themes/<?php echo $theme['url-wporg']; ?>" target="_blank">
                         <img src="<?php echo image_path( 'devices/' . $theme['image'] ); ?>">
                     </a>
                 </div>
                 <div class="large-4 columns">
-                    <a class="desc" href="<?php echo $theme['url-wpcom']; ?>" target="_blank">
-                        <div class="price">$<?php echo $theme['price-wpcom']; ?></div>
+                    <a class="desc" href="<?php echo $theme['url-wporg']; ?>" target="_blank">
+                        <div class="price">$<?php echo $theme['price-wporg']; ?></div>
                         <h3><?php echo $theme['name']; ?></h3>
                         <p><?php echo $theme['description']; ?></p>
                         <div class="button">Demo &amp; Details</div>
