2596ee48456d7fbeb7a0017c258cd068acb844bf
BinaryMoon <binarymoon@gmail.com>
2015-11-01 12:51:36 +0000

    Add javascript to support details

--- views/_tools/_support-details/index.php
+++ views/_tools/_support-details/index.php
@@ -7,26 +7,37 @@
             'name' => 'IP Address',
             'icon' => 'map-marker',
             'value' => sd_get_ip_address(),
+            'description' => 'An IP Address (Internet Protocol address) is a numerical label assigned to each device (e.g., computer, printer) in a computer network. This can be useful for distinguishing different machines.',
         ),
         'country' => array(
             'name' => 'Location',
             'icon' => 'globe',
             'value' => sd_get_location(),
+            'description' => 'The location property may not be 100% accurate, especially the city element which is often wrong. However it is useful for seeing if there is anyting wildly wrong with your computer settings (eg showing if you are in the wrong country).',
         ),
         'browser' => array(
             'name' => 'Web Browser',
             'icon' => sd_get_browser_icon(),
             'value' => sd_get_browser(),
+            'description' => 'A web browser is the application you use to view the internet. Knowing exactly what this is makes it much easier for us to duplicate bugs that only happen on a specific device.'
         ),
         'browser-dimensions' => array(
             'name' => 'Browser Dimensions',
             'icon' => 'arrows-alt',
             'value' => 'n/a',
+            ''
         ),
         'platform' => array(
             'name' => 'Platform',
             'icon' => 'laptop',
             'value' => sd_get_platform(),
+            'description' => 'The platform is either the device/ operating system you are using.'
+        ),
+        'javascript' => array(
+            'name' => 'Javascript',
+            'icon' => 'code',
+            'value' => 'disabled',
+            'description' => 'Soeme websites required javascript enabled to function correctly.'
         ),
         /**
         '' => array(
@@ -41,6 +52,9 @@
     <?php site_breadcrumbs(); ?>
 
     <h1>Computer Details</h1>
+    <p class="intro">The information on this page makes it easier for web developers to debug issues with websites.</p>
+    <p>The more information we know, the easier it is for us to reproduce bugs, since problems that happen on one computer, may not happen on another.</p>
+    <p><strong>Note</strong> no personal information is stored.</p>
 </div>
 
 <div class="wrapper block-wrapper theme-wrapper">
@@ -49,7 +63,13 @@
 ?>
     <div class="block key-<?php echo $key; ?> invert">
         <div class="content">
+<?php
+        if ( isset( $data['description'] ) ) {
+?>
             <a href="#" class="tip"><i class="fa fa-question-circle"></i></a>
+<?php
+        }
+?>
             <i class="fa fa-<?php echo $data['icon']; ?> icon"></i>
             <h3><?php echo $data['name']; ?></h3>
             <p><?php echo $data['value']; ?></p>
@@ -58,6 +78,20 @@
 <?php
     }
 ?>
+</div>
+
+<div class="wrapper text">
+    <ul>
+<?php
+        foreach ( $properties as $key => $data ) {
+            if ( isset( $data[ 'description' ] ) ) {
+?>
+        <li><strong><?php echo $data['name']; ?></strong> <?php echo $data['description']; ?></li>
+<?php
+            }
+        }
+?>
+    </ul>
 </div>
 <?php
     site_include_view( '_partials/more-themes.php' );
--- views/_tools/_support-details/scripts.js
+++ views/_tools/_support-details/scripts.js
@@ -1,3 +1,4 @@
 ;(function() {
     $( '.key-browser-dimensions p' ).html( window.innerWidth + ' x ' + window.innerHeight );
+    $( '.key-javascript p' ).html( 'enabled' );
 })();
