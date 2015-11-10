5b8936b330fa2058a1b4711efe23f8ff349616bd
BinaryMoon <binarymoon@gmail.com>
2015-11-10 11:36:22 +0000

    Allow circular icons download path to change if theme club is disabled

--- views/_tools/_circular-icons/index.php
+++ views/_tools/_circular-icons/index.php
@@ -1,10 +1,17 @@
+<?php
+    $download_url = path( 'assets/downloads/circular-icons.zip' );
+    if ( ! DISABLE_THEME_CLUB ) {
+        $download_url = 'https://gum.co/EItq';
+    }
+?>
 <div class="wrapper text">
     <?php site_breadcrumbs(); ?>
 
     <h1>Circular Icons</h1>
-    <p class="intro">A bitmap icon set I published in 2008.</p>
-    <img src="<?php echo image_path( 'website/circular-icons.png' ); ?>" alt="Circular Icons Demo" />
-    <a href="<?php echo path( 'assets/downloads/circular-icons.zip' ); ?>" class="button greedy positive"><i class="fa fa-download"></i>Download Circular Icon Set</a>
+    <p class="intro">A free bitmap icon set I first published in 2008. A set of 117 pixel icons in 16x16 size, designed for a variety of uses from websites to apps.</p>
+    <p><img src="<?php echo image_path( 'website/circular-icons.png' ); ?>" alt="Circular Icons Demo" /></p>
+    <p><a href="<?php echo $download_url; ?>" class="button greedy positive"><i class="fa fa-download"></i>Download Circular Icon Set</a></p>
+    <p class="note">Released under the GPL.</p>
 </div>
 
 <?php
