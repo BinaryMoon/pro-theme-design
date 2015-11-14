cc2272ee9fd8938e72ec799004b036499ece3136
BinaryMoon <binarymoon@gmail.com>
2015-11-14 09:14:53 +0000

    tidy purchase buttons

--- views/_partials/theme-purchase-buttons.php
+++ views/_partials/theme-purchase-buttons.php
@@ -1,19 +1,19 @@
+<?php
+    if ( ! empty( $theme[ 'url-wporg' ] ) ) {
+?>
         <section class="wrapper">
             <div class="note message">
                 <h2>Get <?php echo $theme['name']; ?> for WordPress.org</h2>
 
+                <a href="#" class="button fat positive get-theme"><i class="fa fa-download"></i>Get <?php echo $theme['name']; ?> Now!</a>
 <?php
-    if ( ! empty( $theme[ 'url-wporg' ] ) ) {
-?>
-            <a href="#" class="button fat positive get-theme"><i class="fa fa-download"></i>Get <?php echo $theme['name']; ?> Now!</a>
-<?php
-    }
-
-    if ( ! empty( $theme[ 'url-preview' ] ) ) {
+        if ( ! empty( $theme[ 'url-preview' ] ) ) {
 ?>
             <a href="<?php echo $theme[ 'url-preview' ]; ?>" class="button fat ghost"><i class="fa fa-eye"></i>Preview <?php echo $theme['name']; ?></a>
 <?php
-    }
+        }
 ?>
             </div>
         </section>
+<?php
+    }
