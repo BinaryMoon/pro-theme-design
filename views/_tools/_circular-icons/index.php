561a52f20d5248a154cd62837cf354b01d86bb2d
BinaryMoon <binarymoon@gmail.com>
2015-11-14 18:11:14 +0000

    improve circular icons downloads

--- views/_tools/_circular-icons/index.php
+++ views/_tools/_circular-icons/index.php
@@ -9,8 +9,8 @@
 
     <h1>Circular Icons</h1>
     <p class="intro">A free bitmap icon set I first published in 2008. A set of 117 pixel icons in 16x16 size, designed for a variety of uses from websites to apps.</p>
-    <p><img src="<?php echo image_path( 'website/circular-icons.png' ); ?>" alt="Circular Icons Demo" /></p>
-    <p><a href="<?php echo $download_url; ?>" class="button greedy positive"><i class="fa fa-download"></i>Download Circular Icon Set</a></p>
+    <p><a href="<?php echo $download_url; ?>"><img src="<?php echo image_path( 'website/circular-icons.png' ); ?>" alt="Circular Icons Demo" /></a></p>
+    <p><a href="<?php echo $download_url; ?>" class="button fat greedy positive"><i class="fa fa-download"></i>Download Circular Icon Set</a></p>
     <p class="note">Released under the GPL.</p>
 </div>
 
