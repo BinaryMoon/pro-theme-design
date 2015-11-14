67d1226a55a1918d6503d99a98c423c5c6237b1c
BinaryMoon <binarymoon@gmail.com>
2015-11-14 09:09:50 +0000

    Style search results

--- views/_partials/google-search.php
+++ views/_partials/google-search.php
@@ -1,7 +1,7 @@
 
-<form method="get" id="searchform" id="searchbox_<?php echo GOOGLE_CSE; ?>" action="<?php echo path( 'search' ); ?>">
+<form method="get" id="searchform" class="searchform" id="searchbox_<?php echo GOOGLE_CSE; ?>" action="<?php echo path( 'search' ); ?>">
     <input value="<?php echo GOOGLE_CSE; ?>" name="cx" type="hidden"/>
     <input value="FORID:11" name="cof" type="hidden"/>
-    <input type="text" value="" placeholder="Search..." name="q" id="q" class="long-text" />
+    <input type="text" value="" placeholder="Search..." name="q" id="q" class="text" />
     <input type="submit" id="searchsubmit" value="Search" class="button" />
 </form>
