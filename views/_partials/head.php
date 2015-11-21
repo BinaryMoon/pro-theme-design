db033a8c68434c06043c59359c4adaea4974a839
BinaryMoon <binarymoon@gmail.com>
2015-11-21 11:07:25 +0000

    move tracking codes into separate file for simpler management. Also add Facebook pixel tracking

--- views/_partials/head.php
+++ views/_partials/head.php
@@ -15,19 +15,13 @@
         <meta name="google-site-verification" content="VDLz-TZD53CTCmTol81Cd1UoOyz5RuAjdyjiP4irmsk" />
         <meta name="description" content="<?php site_description( '', true ); ?>">
 
-<?php
-    if ( 'prod' == ENV ) {
-?>
         <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-            ga( 'create', 'UA-55722-9', 'auto' );
-            ga( 'send', 'pageview' );
+            var GUMROAD_ACTIVE = '<?php echo (DISABLE_GUMROAD) ? 'true' : 'false'; ?>';
         </script>
 <?php
+    if ( 'prod' == ENV ) {
+        include( 'tracking.php' );
     }
 ?>
+
     </head>
