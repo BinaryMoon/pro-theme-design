586f01af2eef5e9b65f9ef4aeae5ebe41b2e418a
BinaryMoon <binarymoon@gmail.com>
2015-11-22 15:22:58 +0000

    add popover

---
+++ views/_partials/popover.php
@@ -0,0 +1,14 @@
+<?php
+    if ( ! site_popover() ) {
+        return;
+    }
+?>
+
+    <section class="popover">
+        <header>Feedback</header>
+        <a href="#" class="close-button" rel="nofollow"><i class="fa fa-times"></i></a>
+        <div class="body">
+            <p>We'd love to get some feedback to help us create the things you want.</p>
+            <p><a href="<?php echo path( 'feedback/' ); ?>" class="button greedy">Give Feedback</a></p>
+        </div>
+    </section>
