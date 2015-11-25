27deea88546b0ed89ea636fec22ef816077091c4
BinaryMoon <binarymoon@gmail.com>
2015-11-25 09:15:03 +0000

    add a test quiz with initial quizzing code

---
+++ js/vendor/quiz.js
@@ -0,0 +1,257 @@
+/* globals jQuery, window */
+
+;( function( $ ) {
+
+    $.fn.elementalQuiz = function( options ) {
+
+        var defaults = {
+            answers: [],
+            brackets: {},
+            score_suffix: '%',
+            title: 'awesome',
+            share_extras: '',
+            share_url: window.location.href,
+            share_message: 'I got {score} on the {title} quiz!'
+        };
+
+        options = $.extend( defaults, options );
+
+        return this.each( function() {
+
+            var quiz = $( this ),
+                progress = quiz.find( '.quiz-progress div' ),
+                notice = quiz.find( '.quiz-notice' ),
+                controls = quiz.find( '.quiz-controls' ),
+                user_answers = [],
+                questionsAnswered = 0,
+                questionLength = quiz.find( '.quiz-question' ).length;
+
+            /**
+             * Check to see what answers are correct
+             *
+             * @returns {array} A list showing if each question is right or not.
+             */
+            var check_answers = function () {
+
+                // summarize users answers
+                quiz.find( '.quiz-question input[type=radio]:checked' ).each( function() {
+                    user_answers.push( $( this ).data( 'key' ) );
+                } );
+
+                var resultArr = [],
+                    flag = false;
+
+                // check correct answers against users answers
+                for ( var i = 0; i < user_answers.length; i++ ) {
+
+                    flag = false;
+
+                    if ( options.answers[i] === user_answers[i] ) {
+                        flag = true;
+                    }
+
+                    resultArr.push( flag );
+
+                }
+
+                return resultArr;
+
+            };
+
+
+            /**
+             * Round number to specified decimal places
+             *
+             * @param   {float}  num Number to round
+             * @param   {int}    dec Numer of decimal places
+             * @returns {number} Numer to return
+             */
+            var round_decimal = function ( num, dec ) {
+
+                var result = Math.round( num * Math.pow( 10, dec ) ) / Math.pow( 10, dec );
+                return result;
+
+            };
+
+
+            /**
+             * Update the progress bar position and button visibility
+             *
+             * @param {number} change How many questions to change the number by.
+             */
+            var update_progress = function ( change ) {
+
+                questionsAnswered = questionsAnswered + change;
+
+                quiz.find( '.quiz-button-prev' ).show();
+                quiz.find( '.quiz-button-next' ).show();
+                quiz.find( '.quiz-button-finish' ).hide();
+
+                // first question
+                if ( 0 === questionsAnswered ) {
+                    quiz.find( '.quiz-button-prev' ).hide();
+                }
+
+                // last question
+                if ( questionsAnswered === ( questionLength - 1 ) ) {
+                    quiz.find( '.quiz-button-next' ).hide();
+                    quiz.find( '.quiz-button-finish' ).show();
+                }
+
+                // set prograss bar position
+                var percentage = Math.round( ( questionsAnswered + 1) / ( questionLength + 1 ) * 100 );
+                progress.css( 'width', percentage + '%' );
+
+            };
+
+
+            // Change question status when questions are clicked
+            quiz.find( '.answers input' ).click( function() {
+
+                $( this ).parents( '.answers' ).children( 'li' ).removeClass( 'selected' );
+                $( this ).parents( 'li' ).addClass( 'selected' );
+
+            } );
+
+
+            // Start the quiz
+            quiz.find( '.quiz-button-start' ).click( function() {
+
+                // group question inputs so only one can be selected at a time
+                var question = 0;
+
+                quiz.find( '.answers' ).each( function() {
+
+                    $( this ).find( 'input' ).each( function() {
+                        $( this ).attr( 'name', 'question-' + question );
+                    } );
+
+                    question ++;
+
+                } );
+
+                // Ensure none of the quiz options have been checked
+                $( '.quiz .answers input' ).prop( 'checked', false );
+
+                // hide intro
+                $( this ).parents( '.quiz-intro' ).hide();
+
+                // show first question
+                $( '.quiz-question' ).first().addClass( 'active' );
+
+                // show controls
+                controls.show();
+
+                // reset the progress bar
+                update_progress( 0 );
+
+                return false;
+
+            } );
+
+
+            // next question button
+            quiz.find( '.quiz-button-next' ).click( function() {
+
+                // check an answer has been selected
+                var selected_answer = quiz.find( '.quiz-question.active input[type=radio]:checked' );
+
+                if ( 0 === selected_answer.length ) {
+                    notice.show();
+                    return false;
+                }
+
+                // make sure the error notice is hidden
+                notice.hide();
+
+                // move to the next question
+                quiz.find( '.quiz-question.active' ).removeClass( 'active' ).next().addClass( 'active' );
+
+                // update progress bar
+                update_progress( +1 );
+
+                return false;
+
+            } );
+
+
+            // previous question button
+            $( this ).find( '.quiz-button-prev' ).click( function() {
+
+                // hide the error notice
+                notice.hide();
+
+                // move to previous question
+                quiz.find( '.quiz-question.active' ).removeClass( 'active' ).prev().addClass( 'active' );
+
+                // update progress bar
+                update_progress( -1 );
+
+            } );
+
+
+            // finish the quiz and get the results and share now stuff
+            quiz.find( '.quiz-button-finish' ).click( function() {
+
+                // check an answer has been selected
+                var selected_answer = quiz.find( '.quiz-question.active input[type=radio]:checked' );
+
+                if ( 0 === selected_answer.length ) {
+                    notice.show();
+                    return false;
+                }
+
+                // hide interface that's not needed anymore
+                quiz.find( '.quiz-question' ).hide();
+                controls.hide();
+
+                // show results
+                quiz.find( '.quiz-results' ).show();
+
+                var results = check_answers(),
+                    trueCount = 0,
+                    score;
+
+                // see how many answers are correct
+                for ( var i = 0; i < results.length; i++ ) {
+                    if ( true === results[i] ) {
+                        trueCount ++;
+                    }
+                }
+
+                // calculate percentage correct and apply to results screen
+                score = round_decimal( trueCount / questionLength * 100, 2 );
+                quiz.find( '.quiz-score' ).html( score + options.score_suffix );
+
+                // display brackets
+                var bracket = '';
+                for ( var key in options.brackets ) {
+                    if ( score >= key ) {
+                        bracket = options.brackets[ key ];
+                    } else {
+                        break;
+                    }
+                }
+
+                $( '.quiz-bracket-' + bracket ).show();
+
+                // social sharing
+
+                var share_message = options.share_message;
+                share_message = share_message.replace( '{title}', options.title );
+                share_message = share_message.replace( '{score}', score + options.score_suffix );
+                share_message = share_message.replace( '{url}', options.share_url );
+
+                $( '.quiz-share-twitter' ).attr( 'href', 'https://twitter.com/intent/tweet?text=' + encodeURI( share_message ) + '&url=' + encodeURI( options.share_url ) + options.share_extras  );
+                $( '.quiz-share-google' ).attr( 'href', 'https://plus.google.com/share?url=' + '' );
+                $( '.quiz-share-facebook' ).attr( 'href', 'https://www.facebook.com/sharer/sharer.php?u=' + '' );
+
+                return false;
+
+            } );
+
+        } );
+
+    };
+
+} )( jQuery );
---
+++ views/_tools/_quiz-test/index.php
@@ -0,0 +1,134 @@
+<?php
+    site_script( path( 'js/vendor/quiz.js' ) );
+    site_script( path( 'views/_tools/_quiz-test/scripts.min.js' ) );
+?>
+<div class="quiz wrapper text">
+
+    <div class="quiz-intro note message">
+
+        <h1>Test Quiz</h1>
+        <a class="quiz-button-start button fat" href="#">Start</a>
+
+    </div>
+
+    <div class="quiz-question">
+        <div class="question">
+            CSS is used to...
+        </div>
+        <ul class="answers">
+            <li>
+                <label><input data-key="a" type="radio">lend structure to the document</label>
+            </li>
+            <li>
+                <label><input data-key="b" type="radio">mold the presentation of the document</label>
+            </li>
+            <li>
+                <label><input data-key="c" type="radio">script the interactions on the page</label>
+            </li>
+            <li>
+                <label><input data-key="d" type="radio">You're crafty! This is a trick question.</label>
+            </li>
+        </ul>
+    </div>
+
+    <div class="quiz-question">
+        <div class="question">
+            The C in CSS stands for?
+        </div>
+        <ul class="answers">
+            <li>
+                <label><input data-key="a" type="radio">Crysis</label>
+            </li>
+            <li>
+                <label><input data-key="b" type="radio">Crocodile</label>
+            </li>
+            <li>
+                <label><input data-key="c" type="radio">Consistent</label>
+            </li>
+            <li>
+                <label><input data-key="d" type="radio">Cascading</label>
+            </li>
+        </ul>
+    </div>
+
+    <div class="quiz-question">
+        <div class="question">
+            The correct way to link to a stylesheet in the head is?
+        </div>
+        <ul class="answers">
+            <li>
+                <label><input data-key="a" type="radio">&lt;link rel="stylesheet" href="styles.css" /&gt;</label>
+            </li>
+            <li>
+                <label><input data-key="b" type="radio">&lt; stylesheet src="styles.css"&gt;</label>
+            </li>
+            <li>
+                <label><input data-key="c" type="radio">&lt; link src="styles.css"&gt;</label>
+            </li>
+            <li>
+                <label><input data-key="d" type="radio">&lt; css href="styles.css"&gt;</label>
+            </li>
+        </ul>
+    </div>
+
+    <div class="quiz-question">
+        <div class="question">
+            Which of these is not a valid CSS selector?
+        </div>
+        <ul class="answers">
+            <li>
+                <label><input data-key="a" type="radio">#x</label>
+            </li>
+            <li>
+                <label><input data-key="b" type="radio">.x</label>
+            </li>
+            <li>
+                <label><input data-key="c" type="radio">x ) y</label>
+            </li>
+            <li>
+                <label><input data-key="d" type="radio">x:first-child</label>
+            </li>
+        </ul>
+    </div>
+
+    <div class="quiz-results">
+        <h1>You Scored <span class="quiz-score">0%</span></h1>
+
+        <div class="quiz-bracket-wizard quiz-bracket">Wizard</div>
+        <div class="quiz-bracket-genius quiz-bracket">Genius</div>
+        <div class="quiz-bracket-average quiz-bracket">average</div>
+        <div class="quiz-bracket-poor quiz-bracket">poor</div>
+        <div class="quiz-bracket-beginner quiz-bracket">beginner</div>
+
+        <a href="" class="quiz-share-twitter quiz-share button">
+            <i class="fa fa-twitter"></i>
+            <span>Share on Twitter</span>
+        </a>
+        <a href="" class="quiz-share-facebook quiz-share button">
+            <i class="fa fa-facebook"></i>
+            <span>Share on Facebook</span>
+        </a>
+        <a href="" class="quiz-share-google quiz-share button">
+            <i class="fa fa-google-plus"></i>
+            <span>Share on Google+</span>
+        </a>
+    </div>
+
+    <div class="quiz-controls">
+        <div class="quiz-controls-wrapper">
+            <a class="quiz-button-prev button ghost" href="#"><i class="fa fa-arrow-left"></i>Previous</a>
+
+            <div class="quiz-progress">
+                <div></div>
+            </div>
+
+            <a class="quiz-button-next button" href="#">Next <i class="fa fa-arrow-right"></i></a>
+            <a class="quiz-button-finish button" href="#">Finish <i class="fa fa-arrow-right"></i></a>
+        </div>
+    </div>
+
+    <div class="quiz-notice note">
+        Please select an option
+    </div>
+
+</div>
