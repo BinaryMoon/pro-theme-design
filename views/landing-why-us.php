17700a2d7ce898e7f0df2fbd6efab0fb18ace562
BinaryMoon <binarymoon@gmail.com>
2015-12-07 11:06:54 +0000

    'Why us?' landing page

--- views/landing-why-us.php
+++ views/landing-why-us.php
@@ -3,6 +3,7 @@
 
 // http://chrislema.com/marketing-your-software-product/
 // http://www.miva.com/pro
+
 ?>
 
     <body class="page-theme-club">
@@ -20,23 +21,24 @@
                 <li>Why cheap themes can be a big cost in the long term.</li>
             </ul>
 
-            <p class="intro">Small businesses like yours, have successfully used Pro Theme Design themes to improve their websites, or their clients websites, for many years now. We realeased our first theme in 2007 and have been regularly releasing new themes ever since.</p>
+            <p class="intro">Small businesses like yours, have successfully used Pro Theme Design themes to improve their websites, or their clients websites, for many years now. We released our first theme in 2007 and have been regularly releasing new themes ever since.</p>
 
             <h2>Pro Theme Design is Unique</h2>
 
-            <p>We started in 2007, and have been working with WordPress ever since. The benefit of being small is that we can give things the personal touch. In fact when you contact us you will talk directly to me. The lead developer. Who better to answer your questions about the theme than the person who made it!</p>
+            <p>We started in 2007, and have been working with WordPress ever since. The benefit of being small is that we can give things the personal touch. In fact when you contact us you will talk directly to me. The lead developer. Who better to answer your questions about your new theme than the person who made it!</p>
 
             <p>Another unique thing about us is that all of our themes have been code reviewed by a third party. What this means is that an independant company has ensured that the code in our themes is secure, and solid. This means our themes are as safe as we can make them, and will run efficiently.</p>
 
             <h2>The WordPress Way</h2>
 
-            <p>In 2014 we made a commitment to working the 'WordPress' way exclusively. This means we always follow best practices and coding standards, and we always use WordPress functionality rather than reinventing the wheel. How does this benefit you though? There's a few ways.</p>
+            <p>In 2014 we made a commitment to <strong>working the 'WordPress' way</strong> exclusively. This means we always follow best practices and coding standards, and we always use WordPress functionality rather than reinventing the wheel. How does this benefit you though? There's a few ways.</p>
 
-            <p>Firstly - using core WordPress functionality means that as WordPress is updated - your site improves, and you don't have to do anything (besides update WordPress).</p>
+            <ol>
+                <li>Using core WordPress functionality means that as WordPress is updated - your site, and theme, improves; and you don't have to do anything (besides update WordPress).</li>
+                <li>Following WordPress coding standards means that our themes very rarely break when there is a WordPress update. The less WordPress functionality a theme uses, the more likely it will be to break when there is a WordPress update.</li>
+            </ol>
 
-            <p>Secondly - following WordPress coding standards means that our themes very rarely break when there is a WordPress update. Many themes that try to be clever and do things differently have issues when WordPress changes how it works internally. This isn't nice for the people using the themes as it means their site is more likely to break after an update.</p>
-
-            <h2>The 80% rule</h2>
+            <h2>The 80% Rule</h2>
 
             <p>Customer feedback is important to us. Whether it's keeping notes on common support queries, or reading tweets about WordPress themes, we like to keep on top of the latest developments and feedback. Doing this allows us to learn and improve.</p>
 
@@ -46,17 +48,19 @@
 
             <h2>The Cost of Cheap Themes</h2>
 
-            <p>Developing WordPress themes is an expensive business. On average it takes us 3 months to build each theme that we sell. In addition - for each theme that we sell we have to test with new WordPress releases, fix bugs, add and improve the theme features, and offer support to customers of the theme. We also ensure that all of our themes are code reviewed - which means the code is the best it can be meaning a more reliable website for our customers.</p>
+            <p>Developing WordPress themes is an expensive business. On average it takes us 3 months to build each theme that we sell. In addition we have to test with new WordPress releases, fix bugs, add and improve the theme features, and offer support to customers of the theme. We also ensure that all of our themes are code reviewed - which means a more reliable website for our customers.</p>
 
             <p>This is why cheap themes are bad. If you buy a theme for $20 then the theme developer is unlikely to cover their expenses. So they cut corners. Support for cheap themes is generally of poor quality, themes are not tested, and code is not optimized. These things mean a poor experience for the customer.</p>
 
-            <p>They mean that support issues will take a long time to resolve (or not be resolved at all), and that significant security issues/ site hacks could occur due to the poor quality code.</p>
+            <p>To save time cheaper themes often bundle third party scripts and plugins. However this means that the developers do not understand the code as well as they might. In addition - there have been some well publicised security flaws in a number of third party plugins that theme developers have bundled - which makes their customers more prone to security problems.</p>
 
-            <h2>The benefit of simplicity</h2>
+            <h2>The Benefit of Simplicity</h2>
 
             <p>Some of the most popular WordPress themes are 'all in one' themes. Whilst they look nice on the outside, and in the theme demos, they generally hide a miriad of problems. By trying to create a single theme that does everything for everyone, the theme developer generally makes a lot of complex code that is hard to maintain and hard to customise.</p>
 
-            <p>For the customer, this means that they will often struggle to get their website to look as nice as the theme demo. It also means that if they want to make a change that the theme does not support - then they will have to spend a lot of money hiring someone to work out how the theme works and then make the change.</p>
+            <p>For the customer, this means that they will often struggle to get their website to look as nice as the theme demo.</p>
+
+            <p>The more complex code in these themes also means that if you were to hire someone to make changes, they would find it a lot harder to do so and so take longer, and cost you more in the long term.</p>
 
             <p>By focusing on simplicity we avoid these problems.</p>
 
@@ -68,7 +72,7 @@
 
             <h2>Supported Plugins</h2>
 
-            <p>To help make our themes simpler we support a number of plugins. This means that we can focus on the theme, and let the plugins do what they do bes.</p>
+            <p>To help make our themes simpler we support a number of plugins. This means that we can focus on the theme, and let the plugins do what they do best.</p>
 
             <ul>
                 <li><strong>Styleguide</strong> - this plugin lets </li>
