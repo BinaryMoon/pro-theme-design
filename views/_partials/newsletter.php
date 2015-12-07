609b4ff4659402a4cdb4b38049e5fbe288fec8e6
BinaryMoon <binarymoon@gmail.com>
2015-12-07 11:06:40 +0000

    Add gum road newsletter form. May or may not use this in the future

--- views/_partials/newsletter.php
+++ views/_partials/newsletter.php
@@ -6,3 +6,14 @@
                     <input type="submit" value="Subscribe" name="subscribe" class="button">
                 </form>
             </section>
+
+<!--
+            <section class="newsletter">
+                <form action="https://gumroad.com/follow_from_embed_form" class="form gumroad-follow-form-embed" method="post">
+                    <h2>Pro Theme Design News Monthly</h2>
+                    <input name="seller_id" type="hidden" value="3110495029003">
+                    <input name="email" placeholder="Your email address" type="email" class="email" required>
+                    <button type="submit" class="button positive">Follow <i class="fa fa-arrow-right end"></i></button>
+                </form>
+            </section>
+-->
