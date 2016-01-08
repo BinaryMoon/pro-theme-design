66b2ee406c3a3fa0cf70118579dbc593131978b3
BinaryMoon <binarymoon@gmail.com>
2016-01-08 10:34:16 +0000

    Improve featured content docs

--- views/_support/_feature/featured-content.php
+++ views/_support/_feature/featured-content.php
@@ -1,11 +1,12 @@
-<p class="intro">A number of Pro Theme Design themes support Featured Content. This is special content, that you select, that gets treated in slightly differently. Each theme behaves differently but often the Featured Content will be used to display the content in a slider/ carousel.</p>
+<p class="intro">A number of Pro Theme Design themes support Featured Content. This is special content, that you select, that is given a special treatment to make it stand out more. Each theme behaves differently but often the Featured Content will be used to display the content in a slider/ carousel.</p>
 
 <p>Featured Content often looks best when used with <?php echo documentation_link( 'featured-image' ); ?>.</p>
 
 <p>To set up Featured Content:</p>
 
 <ol>
-    <li>Go Appearance → Customize → Featured Content</li>
+    <li>Install the Jetpack plugin</li>
+    <li>Go to <em>Appearance → Customize → Featured Content</em></li>
     <li>Enter a tag name for the featured posts — make sure the tag is unique and isn’t used anywhere else on your site</li>
     <li>Edit the posts that you want to display in the Featured Content area and give them the featured tag you have just selected</li>
 </ol>
