e9205652077ef83f3541856c2cc06326f53d9f9e
BinaryMoon <binarymoon@gmail.com>
2016-01-13 10:47:56 +0000

    add opti documentation

---
+++ views/_support/_theme/opti.php
@@ -0,0 +1,30 @@
+<h2>Homepage Layout</h2>
+
+<img src="<?php echo image_path( 'docs/layout-opti.png' ); ?>" alt="Opti Homepage Layout" />
+
+<ol>
+    <li><strong>Featured Content</strong> This shows either the latest post published or, optionally, 4 Featured Posts in a slider. If you want the slider then the 4 featured posts are controlled with <?php echo documentation_link( 'featured-content' ); ?> functionality.</li>
+    <li><strong>Featured Categories</strong> The 4 Featured Categories can be selected in the Theme Settings. If there are no featured categories selected then the theme defaults to using the 3 categories that contain the most blog posts. You can also hide the featured categories in the Theme Settings.</li>
+</ol>
+
+<h2>Theme Settings</h2>
+
+<p>The theme settings control the content displayed on the homepage. These settings can be found in <em>WordPress Admin &rarr; Appearance &rarr; Theme Options</em>.</p>
+
+<img src="<?php echo image_path( 'docs/settings-opti.png' ); ?>" alt="Opti Theme Settings" />
+
+<ul>
+    <li><strong>Display Homepage Categories</strong> - Tick this to hide the Featured Categories on the homepage.</li>
+    <li><strong>Homepage Categories</strong> - Select the categories you want to display in the Featured Categories section on the homepage.</li>
+    <li><strong>Display Header Image on</strong> - Select which pages you would like to display the header image on.</li>
+    <li><strong>Featured Posts</strong> - Select the number of featured posts to display in each of the featured categories.</li>
+    <li><strong>Display Related Posts</strong> - Decide whether you want to display the related posts or not on blog posts.</li>
+</ul>
+
+<h2>Sidebars</h2>
+
+<p>Opti has 5 optional sidebars. They will only display if they contain widgets.</p>
+
+<p>The sidebar widgets will display on all pages. Either in the left or right sidebar, depending upon the page type.</p>
+
+<p>The footer sidebars will display, one after the other, in columns in the footer of the site.</p>
