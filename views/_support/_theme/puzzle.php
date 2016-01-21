f35483445c0b05d938d56b99d8457c65da990bd9
BinaryMoon <binarymoon@gmail.com>
2016-01-21 16:36:36 +0000

    improve puzzle documentation

---
+++ views/_support/_theme/puzzle.php
@@ -0,0 +1,68 @@
+<h2>Customizer Settings</h2>
+
+<img src="<?php echo image_path( 'docs/customizer-puzzle.png' ); ?>" alt="Mimbo Pro Cusomtizer Screenshot" class="align-right"/>
+
+<p>Puzzle has 2 settings in the Customizer. The first changes the homepage and archive page layouts, the second disables the 'recent posts' section that displays underneath blog posts.</p>
+
+<h2>Homepage Layouts</h2>
+
+<p>The available homepage layouts are listed below. Different layouts are suitable for different types of content, and allow different amounts of content to appear at a glance.</p>
+
+<p>The order can be confusing, but in general blog posts display from left to right, and then fill the next highest available position that they fit in.</p>
+
+</div>
+
+<div class="wrapper block-wrapper theme-wrapper">
+    <div class="block showcase-website">
+        <img src="<?php echo image_path( 'docs/puzzle-layout-1.png' ); ?>" alt="Puzzle Layout sample" class="image" />
+        <div class="meta">
+            <h3>Puzzle Layout</h3>
+        </div>
+    </div>
+    <div class="block showcase-website">
+        <img src="<?php echo image_path( 'docs/puzzle-layout-2.png' ); ?>" alt="Brick Layout sample" class="image" />
+        <div class="meta">
+            <h3>Brick Layout</h3>
+        </div>
+    </div>
+    <div class="block showcase-website">
+        <img src="<?php echo image_path( 'docs/puzzle-layout-3.png' ); ?>" alt="5 Column Grid sample" class="image" />
+        <div class="meta">
+            <h3>5 Column Grid</h3>
+        </div>
+    </div>
+    <div class="block showcase-website">
+        <img src="<?php echo image_path( 'docs/puzzle-layout-4.png' ); ?>" alt="3 Column Grid sample" class="image" />
+        <div class="meta">
+            <h3>3 Column Grid</h3>
+        </div>
+    </div>
+    <div class="block showcase-website">
+        <img src="<?php echo image_path( 'docs/puzzle-layout-5.png' ); ?>" alt="5 Column Portrait Grid sample" class="image" />
+        <div class="meta">
+            <h3>5 Column Portrait Grid</h3>
+        </div>
+    </div>
+    <div class="block showcase-website">
+        <img src="<?php echo image_path( 'docs/puzzle-layout-6.png' ); ?>" alt="3 Column Portrait Grid sample" class="image" />
+        <div class="meta">
+            <h3>3 Column Portrait Grid</h3>
+        </div>
+    </div>
+    <div class="block showcase-website">
+        <img src="<?php echo image_path( 'docs/puzzle-layout-7.png' ); ?>" alt="5 Column Grid sample" class="image" />
+        <div class="meta">
+            <h3>Jumble Layout</h3>
+        </div>
+    </div>
+
+</div>
+
+<div class="wrapper text">
+
+
+<h2>Sidebars</h2>
+
+<p>Puzzle has 2 optional sidebars. They will only display if they contain widgets.</p>
+
+<p>The bottom sidebar is on all pages and displays in the footer. The sidebar widgtes display in the right hand sidebar on all blog posts and pages - apart from those using the Full Width template.</p>
