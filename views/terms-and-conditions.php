66d1418ac8e06a021f65fdef6c539507490ee443
BinaryMoon <binarymoon@gmail.com>
2016-01-21 17:50:46 +0000

    improve terms
    tidy code
    clarify theme transfer policy
    add links to relevant support locations for theme purchases

--- views/terms-and-conditions.php
+++ views/terms-and-conditions.php
@@ -8,9 +8,12 @@
 
         <div class="wrapper text">
             <h1>Terms and Conditions</h1>
+
             <h2>Support</h2>
-            <p>We offer support via the relevant support forums. For wordpress.com please use the premium themes forum. For self hosted themes please use the support form on the relevant theme page on Creative Market.</p>
-            <p>The support we offer is restricted to fixing bugs in our themes. Unfortunately we don't help with theme modifications/ customisations or installation issues. For help with customizations we have partnered with Codable who offer <a href="<?php echo path( 'theme-customization/' ); ?>">customization services for WordPress websites</a>.</p>
+
+            <p>We offer support via the relevant support forums. For wordpress.com please use the <a href="https://premium-themes.forums.wordpress.com">premium themes forum</a>. For self hosted themes please use the support form on the relevant theme page on <a href="https://creativemarket.com/BinaryMoon?u=BinaryMoon">Creative Market</a>.</p>
+
+            <p>The support we offer is restricted to fixing bugs in our themes. Unfortunately we don't help with theme modifications/ customisations or installation issues. For help with customizations we have partnered with Codeable who offer <a href="<?php echo path( 'theme-customization/' ); ?>">customization services for WordPress websites</a>.</p>
 
             <p>We do not offer support for problems with third party plugins/ modifications.</p>
 
@@ -23,20 +26,26 @@
             <p>All of our products are licensed under the same license as WordPress, the GPL – there is more information on the <a href="http://codex.wordpress.org/GPL">GPL license</a> available on the WordPress Codex. Support for Pro Theme Design products will only be given to those who purchase a Pro Theme Design product.</p>
 
             <h2>Multiple Sites</h2>
+
             <p>You are welcome to use our themes on as many sites as you like.</p>
 
             <h2>Individual Theme Support</h2>
+
             <p>Theme support is offered for the life of the product. If a product is retired from sale then support will continue for a further 6 months. After this support will no longer be given for this product.</p>
+
             <p>Products are considered active if they are available on the site. We reserve the right to retire a product at any time without notice.</p>
 
             <h2>Theme Club Support</h2>
+
             <p>Theme Club support is offered for the duration of the theme club subscription. It will only be offered for themes that are currently active in the theme club. We reserve the right to retire a theme from the theme club at any time without notice.</p>
 
             <h2>Product Updates</h2>
+
             <p>We do not schedule, or guarantee, product updates however we do update products and will notify customers when we do so. Updates for minor versions will be free. Occassionally we will release a major update to a product and we reserve the right to charge an upgrade fee at our discretion.</p>
 
             <h2>Transferring themes from wordpress.com</h2>
-            <p>Unfortunately themes purchased on WordPress.com are separate products and so are non transferable. This is detailed on the <a href="http://en.support.wordpress.com/themes/premium-themes/">premium theme terms on wordpress.com</a>.</p>
+
+            <p>Unfortunately themes purchased on WordPress.com are separate products and so are non transferable. This is detailed on the <a href="http://en.support.wordpress.com/themes/premium-themes/">premium theme terms on wordpress.com</a>. If you wish to move from wordpress.com to a self hosted site then you will need to repurchase your theme.</p>
         </div>
 
 
