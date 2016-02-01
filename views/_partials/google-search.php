b67f749a7a739c86b6ffdebe5998fbbe65cb4044
BinaryMoon <binarymoon@gmail.com>
2016-02-01 15:23:57 +0000

    make search overlay a modal

--- views/_partials/google-search.php
+++ views/_partials/google-search.php
@@ -1,7 +1,7 @@
 
 <form method="get" class="searchform" id="searchbox_<?php echo GOOGLE_CSE; ?>" action="<?php echo path( 'search' ); ?>">
-    <input value="<?php echo GOOGLE_CSE; ?>" name="cx" type="hidden"/>
-    <input value="FORID:11" name="cof" type="hidden"/>
-    <input type="text" value="" placeholder="Search..." name="q" id="q" class="text" />
+    <input value="<?php echo GOOGLE_CSE; ?>" name="cx" type="hidden" />
+    <input value="FORID:11" name="cof" type="hidden" />
+    <input type="text" value="" placeholder="Search..." name="q" id="q" class="text squashy" />
     <button type="submit" id="searchsubmit" class="button"><i class="fa fa-search solo"></i><span class="screen-reader-text">Search</span></button>
 </form>
