76bd50a3632ec57754f8ee0247f9ec2c5163260e
BinaryMoon <binarymoon@gmail.com>
2016-02-01 15:23:47 +0000

    add nav to header

--- views/_partials/header.php
+++ views/_partials/header.php
@@ -5,6 +5,7 @@
     }
 ?>
         <header class="masthead" id="masthead">
+            <?php site_include_view( '_partials/nav.php' ); ?>
             <div class="wrapper">
                 <<?php echo $h; ?> class="site-title">
                     <a href="<?php echo path(); ?>">
@@ -13,27 +14,5 @@
                 </<?php echo $h; ?>>
                 <p class="animated fadeInUp"><?php site_description(); ?></p>
             </div>
-
-            <nav class="social">
-                <a class="twitter" href="https://www.twitter.com/prothemedesign">
-                    <i class="fa fa-twitter"></i>
-                    <span class="screen-reader-text">Follow us on Twitter</span>
-                </a>
-                <a class="facebook" href="https://www.facebook.com/prothemedesign">
-                    <i class="fa fa-facebook"></i>
-                    <span class="screen-reader-text">Follow us on Facebook</span>
-                </a>
-            </nav>
-
-            <nav class="utils">
-                <a href="#main-menu" class="menu-toggle">
-                    <i class="fa fa-bars"></i>
-                    <span class="screen-reader-text">Go to menu</span>
-                </a>
-                <a href="#search" class="search-toggle">
-                    <i class="fa fa-search"></i>
-                    <span class="screen-reader-text">Search Pro Theme Design</span>
-                </a>
-            </nav>
         </header>
 
