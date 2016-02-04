9e495eedff0b5824167461eee7671935dbd30565
BinaryMoon <binarymoon@gmail.com>
2016-02-04 14:58:53 +0000

    add extra image to circular icons page

--- views/_tools/_circular-icons/index.php
+++ views/_tools/_circular-icons/index.php
@@ -13,6 +13,7 @@
 
     <h1>Circular Icons Pro</h1>
     <p class="intro">A comprehensive icon set that includes more than 120 pixel icons in a variety of sizes from 16x16 up to 128x128, designed for a variety of uses from websites to apps. There are three icon packs available - all have the same icons in them, but some they include different file formats.</p>
+    <p><a href="<?php echo $download_url; ?>"><img src="<?php echo image_path( 'website/circular-icons-screenshot.png' ); ?>" alt="Circular Icons Pro Large Screenshot" /></a></p>
     <ul>
         <li><strong>PNG Pack (free)</strong>, includes icons in 4 sizes (16x16, 32x32, 64x64, and 128x128).</li>
         <li><strong>SVG Pack ($10)</strong>, includes pngs, and svgs for all icons (the cd icon does not work as an svg).</li>
