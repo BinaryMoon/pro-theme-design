76af7e1fc80427445020d7a8cd29ff5939a1c700
BinaryMoon <binarymoon@gmail.com>
2016-02-08 16:11:01 +0000

    Tweak contact thanks message

--- views/contact-thanks.php
+++ views/contact-thanks.php
@@ -6,8 +6,8 @@
         <?php include( '_partials/header.php' ); ?>
 
         <div class="wrapper text">
-            <h2>Thanks for the message. We'll get back to you as soon as we can.</h2>
-            <p class="intro">Please note we don't have advertising on Pro Theme Design and will ignore any messages about it.</p>
+            <h2>Thanks for the message. We'll get back to you if needed.</h2>
+            <p>Please note we don't have advertising on Pro Theme Design and will ignore any messages about it.</p>
         </div>
 
 <?php
