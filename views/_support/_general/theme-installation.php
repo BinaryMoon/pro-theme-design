7e5d9b236b108ab039be0451c713b7d0c84b9925
BinaryMoon <binarymoon@gmail.com>
2016-02-20 15:35:38 +0000

    Improve theme installation documentation

--- views/_support/_general/theme-installation.php
+++ views/_support/_general/theme-installation.php
@@ -1,6 +1,10 @@
 <p class="intro">After a successful theme purchase you will receive an email including your individual theme or theme bundle download link. With this link you can download the zip file that contains your theme files.</p>
 
-<p class="note">Please note that on Mac OSX zip files get extracted by default - so check your downloads folder for the zip file before continuing.</p>
+<p class="note tip">On Mac OSX zip files get extracted by default - so check your downloads folder for the zip file before continuing.</p>
+
+<h2>Installation on WordPress.com</h2>
+
+<p>Themes purchased on WordPress.com do not need to be installed. As soon as you purchase as theme it will be available to use through the theme selector in your websites admin area.</p>
 
 <h2>Installation via the WordPress admin panel</h2>
 
