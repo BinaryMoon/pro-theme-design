b80956c39b3a097ee744829f845a2c8b7c0c923e
BinaryMoon <binarymoon@gmail.com>
2016-02-26 17:03:06 +0000

    More documentation for WordPress implementors

--- views/_articles/0002-be-a-wordpress-implementer.php
+++ views/_articles/0002-be-a-wordpress-implementer.php
@@ -21,15 +21,35 @@
 
 <h2>Tips to get started as a WordPress Implementer</h2>
 
-<p></p>
-
 <ul>
     <li><strong>Have a preferred web host</strong> - preferably one with an affiliate program. Learn how to set up WordPress on this host. Get your clients to sign up to this (through your affiliate link) and you can get up and running quickly without having to learn a new system.</li>
     <li><strong>Have a contract ready</strong> - create a good contract that protects you should something go wrong. Define payment terms, who owns the content, and get out clauses. It's worth asking for money up front too. 50% on start of work and 50% on completion is common.</li>
     <li><strong>Have a website</strong> - somewhere to promote yourself, and show off your portfolio of clients.</li>
     <li><strong>Have a collection of 'go to' themes that you know well</strong> - make sure you know a few flxible themes inside out that you can customise easily. This will save you time and effort in the long run.</li>
     <li><strong>Prepare some commonly used plugins</strong> - like the themes, prepare a pack of plugins that cover commonly requested features that you can quickly install and use for your clients.</li>
-    <li><strong>Get updatee processes in place</strong> - using a service like ManageWP can save you a lot of time and effort when managing multiple WordPress clients.</li>
+    <li><strong>Get update processes in place</strong> - using a service like <a href="http://managewp.com">ManageWP</a> can save you a lot of time and effort when managing multiple WordPress clients.</li>
+</ul>
+
+<h2>Development Tips for WordPress Implementors</h2>
+
+<ol>
+    <li><strong>Always use child themes - don't modify the themes you buy.</strong> This is super important. If you modify the themes then you will have a lot of trouble when theme updates are published.</li>
+    <li><strong>Don't modify WordPress</strong> - as with the themes, if you edit WordPress, then you won't be able to update the site without making the changes again. If you want to edit WordPress behaviour then you will need to find (or create) a plugin.</li>
+    <li><strong>Develop locally - then sync to your production site</strong> - often called 'Ninja Coding', working directly on a live server is asking for trouble. It is well worth learning how to use something like MAMP, or XAMPP - or even something more modern like VVV. These things sound complex, but they make development considerably easier.</li>
+    <li><strong>Don't use the WordPress file editor</strong> - using it means that you're (probably) breaking tips 1 and 3 above. You're editing core theme files, on a live server. Not only is it slower - but it opens you up to all sorts of potential breakage.</li>
+    <li><strong>Learn HTML, CSS, PHP, JS (in that order)</strong> - this can sound intimidating - but the more you use them, the more confident you will feel, and the more you will be able to offer your clients.</li>
+    <li><strong>Keep WordPress, Themes, and Plugins up to date</strong> - really. Do it. Security is very important, you don't want your clients to get hacked, so make sure you keep everything up date.</li>
+</ol>
+
+<h2>Improving WordPress Development Skills</h2>
+
+<p>Most WordPress Implementors have some development skills, but it's always a good idea to keep them up to date. Below are a few resources that will help your WordPress skills stay fresh.</p>
+
+<ul>
+    <li><a href="http://codex.wordpress.org">The Codex</a> - a classic, and still the best. This has loads of information about WordPress and how to develop for it.</li>
+    <li><a href="http://developers.wordpress.org">WordPress Developers</a> - this is going to be a replacement for the Codex, but currently it's not as good. There are however some useful elements and it will only get better with time.</li>
+    <li><a href="http://www.wptherightway.org/en/index.html">WordPress the Right Way</a> - a guide on best practices for developing with WordPress. Less about code, and more about the best way to do things.</li>
+    <li><a href="http://www.wptherightway.org/en/code_style_guide/index.html">WordPress Coding Styleguide</a> - this document is part of the 'WordPress the Right Way' docs, but it collects the standards together. It's well worth following the standards since it makes transferring code between developers much simpler.</li>
 </ul>
 
 <h2>The Best of Both Worlds?</h2>
