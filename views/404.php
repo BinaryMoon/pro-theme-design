07b11bfe5f4a35ada56eb2f8223ae89b7ca406f6
BinaryMoon <binarymoon@gmail.com>
2016-03-02 18:03:03 +0000

    Add search to 404 page

--- views/404.php
+++ views/404.php
@@ -6,5 +6,14 @@
 
 <?php
     include( '_partials/header.php' );
+?>
 
+        <div class="wrapper text">
+            <h1>Maybe Searching Will Help?</h1>
+<?php
+    include( '_partials/google-search.php' );
+?>
+        </div>
+
+<?php
     include( '_partials/footer.php' );
