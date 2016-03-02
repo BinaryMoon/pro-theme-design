c4ef93a70433b60a151b7831464f11d150869ae4
BinaryMoon <binarymoon@gmail.com>
2016-03-02 20:19:38 +0000

    more notes for seo post

---
+++ views/_articles/007-improve-seo-for-wordpress-websites.php
@@ -0,0 +1,26 @@
+WP SEO Plugin
+
+Fast efficient themes
+
+Optimize page loading (https, cdn, minify - cloudflare)
+
+Minimal plugins
+
+Build Links
+
+Create shareable content
+
+Social Media (tell people about shareable content)
+
+XML Sitemaps and Webmaster tools
+
+Optimise images
+
+http://www.screamingfrog.co.uk/seo-spider/
+
+Ensure all links have link text and alt text
+
+Minimise use of Javascript around content
+
+Use nofollow on links that are advertising/ not directly relevant to your website
+
