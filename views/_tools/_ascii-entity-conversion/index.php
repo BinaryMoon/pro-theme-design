b9b61058197119e0cd2fbd99bab5a9a8edb0503b
BinaryMoon <binarymoon@gmail.com>
2016-03-18 17:59:27 +0000

    remove double wrapper

--- views/_tools/_ascii-entity-conversion/index.php
+++ views/_tools/_ascii-entity-conversion/index.php
@@ -4,42 +4,40 @@
 <div class="wrapper text">
     <?php site_breadcrumbs(); ?>
 
-    <div class="wrapper text">
+    <h1>Entity Conversion Calculator</h1>
 
-        <h1>Entity Conversion Calculator</h1>
+    <form class="entity-calculator">
+        <label>
+            <span>Paste a character to convert here:</span>
+            <input type="text" id="reverse" maxlength="1" />
+        </label>
+        <button class="button" type="button" id="convert">Convert</button>
+    </form>
 
-        <form class="entity-calculator">
-            <label>
-                <span>Paste a character to convert here:</span>
-                <input type="text" id="reverse" maxlength="1" />
-            </label>
-            <button class="button" type="button" id="convert">Convert</button>
-        </form>
+    <p class="entities">
+        <a href="#" data-entity="&#38">&amp;</a>
+        <a href="#" data-entity="&#169">&copy;</a>
+        <a href="#" data-entity="&#174">&reg;</a>
+        <a href="#" data-entity="&#8592">&larr;</a>
+        <a href="#" data-entity="&#8594">&rarr;</a>
+        <a href="#" data-entity="&#8249">&lsaquo;</a>
+        <a href="#" data-entity="&#8250">&rsaquo;</a>
+        <a href="#" data-entity="&#8226">&bull;</a>
+    </p>
 
-        <p class="entities">
-            <a href="#" data-entity="&#38">&amp;</a>
-            <a href="#" data-entity="&#169">&copy;</a>
-            <a href="#" data-entity="&#174">&reg;</a>
-            <a href="#" data-entity="&#8592">&larr;</a>
-            <a href="#" data-entity="&#8594">&rarr;</a>
-            <a href="#" data-entity="&#8249">&lsaquo;</a>
-            <a href="#" data-entity="&#8250">&rsaquo;</a>
-            <a href="#" data-entity="&#8226">&bull;</a>
-        </p>
+    <p class="note" id="numeric">
+        Numeric Value: <code class="value">?</code>
+        <span class="demo">eg <code>?</code></span>
+    </p>
+    <p class="note" id="css-value">
+        CSS Value: <code class="value">?</code>
+        <span class="demo">eg <code>?</code></span>
+    </p>
+    <p class="note" id="js-value">
+        JS Value: <code class="value">?</code>
+        <span class="demo">eg <code>?</code></span>
+    </p>
 
-        <p class="note" id="numeric">
-            Numeric Value: <code class="value">?</code>
-            <span class="demo">eg <code>?</code></span>
-        </p>
-        <p class="note" id="css-value">
-            CSS Value: <code class="value">?</code>
-            <span class="demo">eg <code>?</code></span>
-        </p>
-        <p class="note" id="js-value">
-            JS Value: <code class="value">?</code>
-            <span class="demo">eg <code>?</code></span>
-        </p>
-    </div>
 </div>
 
 <?php
