e5c18f64d005cbcaad4ab833d4ef012319ae63e2
BinaryMoon <binarymoon@gmail.com>
2016-03-24 11:08:44 +0000

    some notes for 'choose target audience' article

---
+++ views/_articles/008-choose-a-target-audience.php
@@ -0,0 +1,17 @@
+
+
+ - How does choosing a target audience benefit me
+
+It makes it easier to design products
+
+Easier to market products and services
+
+For example when I am designing a new theme I will decide on a niche for the theme. I will then research websites that are in that niche. From that research I will think about the objectives of the people in that niche - and what they want to do. This helps select what custom features are needed.
+
+- How to pick a target niche
+
+- how to create user personas
+
+- how to build a project with user personas
+
+When building the project ask how the thing you're building will benefit your user personas. If it doesn't benefit them, then it's not worth putting time into.
