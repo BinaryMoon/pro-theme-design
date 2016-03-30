53ffe887a3c19dc3b28c3d6c8a1459bc3913ee79
BinaryMoon <binarymoon@gmail.com>
2016-03-30 08:35:51 +0100

    update search input html

--- views/_partials/google-search.php
+++ views/_partials/google-search.php
@@ -2,6 +2,8 @@
 <form method="get" class="searchform" id="searchbox_<?php echo GOOGLE_CSE; ?>" action="<?php echo path( 'search' ); ?>">
     <input value="<?php echo GOOGLE_CSE; ?>" name="cx" type="hidden" />
     <input value="FORID:11" name="cof" type="hidden" />
-    <input type="text" value="" placeholder="Search..." name="q" id="q" class="text squashy" />
-    <button type="submit" id="searchsubmit" class="button"><i class="fa fa-search solo"></i><span class="screen-reader-text">Search</span></button>
+    <input type="text" placeholder="Search..." name="q" id="q" class="text squashy" />
+    <button type="submit" id="searchsubmit" class="button">
+        <i class="fa fa-search solo"></i><span class="screen-reader-text">Search</span>
+    </button>
 </form>
