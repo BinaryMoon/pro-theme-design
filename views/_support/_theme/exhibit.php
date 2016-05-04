b46c464f436a088df9102f751118f6aea29e8834
BinaryMoon <binarymoon@gmail.com>
2016-05-04 12:53:26 +0100

    Add Exhibit Docs

---
+++ views/_support/_theme/exhibit.php
@@ -0,0 +1,18 @@
+<h2>Sidebars</h2>
+
+<p>Exhibit has 2 optional sidebars. They will only display if they contain widgets.</p>
+
+<ul>
+    <li><strong>Sidebar Widgets</strong> - this displays on the right hand side on single blog posts and pages.</li>
+    <li><strong>Footer Widgets</strong> - this displays in the footer and uses javascript to intelligently position the widgets to make sure they slot together nicely.</li>
+</ul>
+
+<h2>Custom Templates</h2>
+
+<p>Exhibit has 3 <a href="<?php echo path( 'documentation/feature/custom-page-templates/' ); ?>">Custom Page Templates</a>. These can be used as different layouts for your websites pages, or as a <a href="<?php echo path( 'documentation/feature/custom-front-page/' ); ?>">Custom Front Page</a>.</p>
+
+<ul>
+    <li><strong>Full Width</strong> - this lets you display your page content full width, with no sidebar.</li>
+    <li><strong>Contributors</strong> - this page displays a list of all the people who have published content on your blog.</li>
+    <li><strong>Projects</strong> - this page displays a list of all the <a href="<?php echo path( 'documentation/feature/portfolio/' ); ?>">projects</a> you have published.</li>
+</ul>
