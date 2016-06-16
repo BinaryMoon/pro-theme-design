6c3895a1863f702784dbf55d284f3943e277147d
BinaryMoon <binarymoon@gmail.com>
2016-06-16 20:59:00 +0100

    improve docs

--- views/_support/_general/transfer-from-wordpress-com.php
+++ views/_support/_general/transfer-from-wordpress-com.php
@@ -1,4 +1,4 @@
-<p class="intro">Themes are not downloadable from WordPress.com - if you wish to use a theme on a self hosted website then you should buy one of our themes from <a href="https://creativemarket.com/BinaryMoon?u=BinaryMoon">Creative Market</a> or <a href="<?php echo path( 'theme-club/' ); ?>">join the theme club</a>.</p>
+<p class="intro">Unfortunately themes are not downloadable from WordPress.com - if you wish to use a theme on a self hosted website then you should buy one of our themes from <a href="<?php echo path( 'themes/wordpress.org/' ); ?>">our site</a> or <a href="<?php echo path( 'theme-club/' ); ?>">join the theme club</a>.</p>
 
 <p>If you've already purchased the theme from wordpress.com then you can't transfer it. This is a standard part of the WordPress.com policy that is mentioned on the <a href="https://en.support.wordpress.com/premium-themes/">WordPress.com premium theme information page</a>.</p>
 
