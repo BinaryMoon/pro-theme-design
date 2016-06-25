f0b6acda67058251b86cb71caf93d3195f0acd6d
BinaryMoon <binarymoon@gmail.com>
2016-06-25 11:06:23 +0100

    move nav

--- views/_partials/header.php
+++ views/_partials/header.php
@@ -5,7 +5,7 @@
     }
 ?>
         <header class="masthead" id="masthead">
-            <?php site_include_view( '_partials/nav.php' ); ?>
+
             <div class="wrapper">
 
                 <<?php echo $h; ?> class="site-title">
@@ -14,7 +14,12 @@
                         <?php site_header_title(); ?>
                     </a>
                 </<?php echo $h; ?>>
+
                 <p class="animated fadeInUp site-description"><?php site_description(); ?></p>
+
             </div>
+
+            <?php site_include_view( '_partials/nav.php' ); ?>
+
         </header>
 
