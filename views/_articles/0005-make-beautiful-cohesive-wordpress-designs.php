1ded8d637909e04020b06cd2864da756ff291e52
BinaryMoon <binarymoon@gmail.com>
2016-06-26 10:38:10 +0100

    update design article

---
+++ views/_articles/0005-make-beautiful-cohesive-wordpress-designs.php
@@ -0,0 +1,46 @@
+<p class="intro">Being an amazing web designer comes naturally to some people - but most people have to work at it. Really hard.</p>
+
+<p>Whilst I really like making new things I find it helpful to have a set of guidelines available to refer to when something doesn't look quite right.</p>
+
+<ol>
+    <li><strong>Line Height</strong> - Line spacing should be set to at least 1.3, around 1.5 is generally good. Don't go too high or text becomes hard to read. <a href="http://mrmrs.io/writing/2015/06/07/line-height/">CSS Line Height</a>.</li>
+    <li><strong>Line length</strong> - too many characters on a line is hard to follow. Aim for 45–90 characters or 2–3 alphabets. <a href="http://typographyforlawyers.com/line-length.html">More on line length</a></li>
+    <li><strong>Whitespace</strong> - the more the merrier. <a href="http://www.seguetech.com/blog/2013/03/20/why-whitespace-important-web-design">More on whitespace</a>.</li>
+    <li><strong>Proximity</strong> - keep related elements nearby. <a href="https://www.smashingmagazine.com/2016/05/improve-your-designs-with-principles-similarity-proximity-part-1/">More on proximity and similarity</a>.</li>
+    <li><strong>Font faces</strong> - use no more than 3 fonts, less than that is better.</li>
+    <li><strong>Font weights</strong> - use different font weights for interest and emphasis.</li>
+    <li><strong>Font size</strong> - use a type scale to ensure beautiful, consistent font sizes. <a href="http://www.modularscale.com/">Create a type scale/ modular scale</a>.</li>
+    <li><strong>Simple, Minimal</strong> - less is more.</li>
+    <li><strong>Contrast fonts (and font sizes)</strong> - for visual interest. <a href="http://casselbear.com/contrast-harmony-personality-pairing-fonts/">Contrast, harmony, and personality in fonts</a>.</li>
+    <li><strong>Contrast colours</strong> - for accessibility. <a href="http://accessibility.psu.edu/color/contrasthtml/">Colour and contrast on web pages</a>.</li>
+    <li><strong>Vertical Rhythm</strong> - creates visual harmony. <a href="http://typecast.com/blog/4-simple-steps-to-vertical-rhythm">More on vertical rhythm</a>.</li>
+    <li><strong>Colour</strong> - keep colours simple. Start with black, white, and 1 key colour. Add more as you need them. Too many colours is confusing. <a href="https://www.smashingmagazine.com/2016/04/web-developer-guide-color/">Web Developers guide to colour</a>.</li>
+    <li><strong>Consistency</strong> - make everything as consistent as possible. Reuse styles in different elements. <a href="http://www.uxpassion.com/blog/the-importance-of-visual-consistency-in-ui-design/">The importance of consistency in web design</a>.</li>
+    <li><strong>Animation</strong> - use animation, use it sparingly. Good animation can be amazing, bad animation can ruin a website. <a href="https://designshack.net/articles/graphics/an-introduction-to-animation-in-web-design/">An introduction to animation in web design</a>.</li>
+    <li><strong>Learn from others</strong> - sites like <a href="https://dribbble.com/BinaryMoon">Dribbble</a> are great for inspiration, don't be afraid to use ideas, but don't copy outright.</li>
+    <li><strong>Use a pattern library</strong> - great for consistency, and testing new functionality. <a href="https://boagworld.com/design/pattern-library/">Why use a pattern library</a>.</li>
+    <li><strong>Letter spacing</strong> - more space for capitals, less space for bold. <a href="http://johndjameson.com/blog/guidelines-for-letterspacing-type/">Simple guidelines on letter spacing</a>.</li>
+    <li><strong>Use Math</strong> - make sizes, padding and spacing multiples of a baseline value. This helps create harmony.<a href="http://jxnblk.com/writing/posts/mathematical-web-typography/">Mathematical web typography</a>.</li>
+</ol>
+
+<h2>Useful Resources</h2>
+
+<ul>
+    <li><a href="http://jonyablonski.com/2015/front-end-principles-for-designers/">Front end principles for designers</a></li>
+    <li><a href="http://practicaltypography.com/">Practical Typography</a></li>
+</ul>
+
+<h2>When Things Aren't Working...</h2>
+
+<p>Below are a few tips to help when a design isn't going as you had hoped.</p>
+
+<ul>
+    <li><strong>Take a break</strong> - take some time off from the project. It's hard to see things objectively when you work closely with something, so the more time you can take away from it, the more clearly you will see the flaws when you return.</li>
+    <li><strong>Clear you head</strong> - go for a walk, get some sleep, watch a film. Anything that will make you forget about the project and think of something else for a while.</li>
+    <li><strong>Get a second opinion</strong> - find someone you can trust to give you honest, constructive feedback. They don't have to be a designer - but it helps if they know about the thing you're designing (ie how to use a website :)).</li>
+    <li><strong>Start from scratch</strong> - sometimes you just have to start again. That doesn't mean you have wasted time though. Often through creating one design you solve a lot of problems, and so your second design can be completed more quickly.</li>
+</ul>
+
+<h2>Practice, Practice, Practice</h2>
+
+<p>The more design you do, the better you will get, however I find having a set of guidelines can help re-align me when things go astray.</p>
