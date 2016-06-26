9e511b15f41075bebe37c8a340ec9a6a931e9aaa
BinaryMoon <binarymoon@gmail.com>
2016-06-26 21:31:13 +0100

    remove wordpress.com from tools page

--- views/_tools/index.php
+++ views/_tools/index.php
@@ -1,8 +1,4 @@
-    <nav class="wrapper page-menu">
-        <a href="<?php echo path( 'tools/' ); ?>" class="tag-tool">Tools</a>
-        <a href="<?php echo path( 'tools/resources/' ); ?>" class="tag-resources">Resources</a>
-        <a href="<?php echo path( 'tools/internal/' ); ?>" class="tag-internal">Internal</a>
-    </nav>
-
 <?php
+    site_page_nav();
+
     tools_display( $tool );
