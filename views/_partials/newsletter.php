af2e170e39750a50fb2b31733d3f341d0ed3a61e
BinaryMoon <binarymoon@gmail.com>
2016-09-18 10:56:49 +0100

    make newsletter signup secure

--- views/_partials/newsletter.php
+++ views/_partials/newsletter.php
@@ -1,5 +1,6 @@
+
             <section class="newsletter">
-                <form action="http://prothemedesign.us5.list-manage2.com/subscribe/post?u=45aef63185bbec7c729059160&amp;id=04983a7171" method="post" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+                <form action="//prothemedesign.us5.list-manage.com/subscribe/post?u=45aef63185bbec7c729059160&amp;id=04983a7171" method="post" name="mc-embedded-subscribe-form" class="validate" target="_blank">
                     <h2>Pro Theme Design News Monthly</h2>
 
                     <label class="screen-reader-text" for="email">Email Address for newsletter messages</label>
@@ -7,14 +8,3 @@
                     <input type="submit" value="Subscribe" name="subscribe" class="button">
                 </form>
             </section>
-
-<!--
-            <section class="newsletter">
-                <form action="https://gumroad.com/follow_from_embed_form" class="form gumroad-follow-form-embed" method="post">
-                    <h2>Pro Theme Design News Monthly</h2>
-                    <input name="seller_id" type="hidden" value="3110495029003">
-                    <input name="email" placeholder="Your email address" type="email" class="email" required>
-                    <button type="submit" class="button positive">Follow <i class="fa fa-arrow-right end" aria-hidden="true"></i></button>
-                </form>
-            </section>
--->
