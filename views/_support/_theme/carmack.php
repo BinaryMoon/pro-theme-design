fb29e3a5b4021849c2a467bb7205b7ae7e78e7c7
BinaryMoon <binarymoon@gmail.com>
2016-09-24 11:50:16 +0100

    update carmack docs

--- views/_support/_theme/carmack.php
+++ views/_support/_theme/carmack.php
@@ -45,12 +45,12 @@
 <img src="<?php echo image_path( 'docs/carmack-post-formats.png' ); ?>" class="align-center" />
 
 <ol>
-    <li><strong>Standard</strong>&nbsp;— The default blog post format&nbsp;displays the content as a news article, with an optional <a href="https://en.support.wordpress.com/featured-images/">featured image</a> positioned on the left.</li>
-    <li><strong>Quote</strong>&nbsp;— The quote post format will highlight the first blockquote found in your blog post. If there’s no blockquote, then it will display the entire blog post.</li>
-    <li><strong>Audio</strong>&nbsp;— The audio post format will highlight the first audio file found in your blog post.</li>
-    <li><strong>Video</strong>&nbsp;— The video post format will display your video and the post title. It supports YouTube, Vimeo, and hosted videos; and the theme will automatically display the first video that’s added to the post content.</li>
-    <li><strong>Image</strong>&nbsp;— The image post format displays a large&nbsp;featured image and the post title. Posts with the image format need a featured image assigned to display properly.</li>
-    <li><strong>Gallery</strong>&nbsp;— Gallery posts will display a gallery of images from the <a href="https://en.support.wordpress.com/gallery/">gallery</a>&nbsp;you’ve added to the post&nbsp;content.</li>
+    <li><strong>Standard</strong> — The default blog post format displays the content as a news article, with an optional featured image positioned on the left.</li>
+    <li><strong>Quote</strong> — The quote post format will highlight the first blockquote found in your blog post. If there’s no blockquote, then it will display the entire blog post.</li>
+    <li><strong>Audio</strong> — The audio post format will highlight the first audio file found in your blog post.</li>
+    <li><strong>Video</strong> — The video post format will display your video and the post title. It supports YouTube, Vimeo, and hosted videos; and the theme will automatically display the first video that’s added to the post content.</li>
+    <li><strong>Image</strong> — The image post format displays a large featured image and the post title. Posts with the image format need a featured image assigned to display properly.</li>
+    <li><strong>Gallery</strong> — Gallery posts will display a gallery of images from the gallery you’ve added to the post content.</li>
 </ol>
 
 <h2>Sidebars</h2>
@@ -68,8 +68,8 @@
 <p>Carmack has 4 <a href="<?php echo path( 'documentation/feature/custom-page-templates/' ); ?>">Custom Page Templates</a>. These can be used as different layouts for your websites pages, or as a <a href="<?php echo path( 'documentation/feature/custom-front-page/' ); ?>">Custom Front Page</a>.</p>
 
 <ul>
-    <li><strong>Contributors </strong>—&nbsp;A list of all of the contributors to your website sorted based on how many posts each person has published. Great for magazines and other multi-author blogs.</li>
+    <li><strong>Contributors</strong> — A list of all of the contributors to your website sorted based on how many posts each person has published. Great for magazines and other multi-author blogs.</li>
     <li><strong>Full-Width Page</strong> — Displays a normal page, but with the sidebars removed so that you can have content that stretches the full width of the site.</li>
     <li><strong>Projects</strong> – Lists all the projects that have been added to your portfolio.</li>
-    <li><strong>Child Page Grid</strong> – Lists all of the pages that are children of the current page. This is handy&nbsp;if you want to create an index of pages.</li>
+    <li><strong>Child Page Grid</strong> – Lists all of the pages that are children of the current page. This is handy if you want to create an index of pages.</li>
 </ul>
