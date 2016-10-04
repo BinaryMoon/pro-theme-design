cf155fdb0ee6b97d5076c273c06ce2191bf223f2
BinaryMoon <binarymoon@gmail.com>
2016-10-04 09:35:45 +0100

    add more notes about the Jetpack social sharing module

--- views/_support/_feature/social-sharing.php
+++ views/_support/_feature/social-sharing.php
@@ -3,3 +3,7 @@
 <h2>Enabling Sharing</h2>
 
 <p>Social Sharing requires the Jetpack plugin to be installed and Activated. Once activated go to the Jetpack settings and enable the Sharing module. You can then go to the Sharing settings which can be found on the WP Admin &rarr; Settings &rarr; Sharing menu.</p>
+
+<p>On the social sharing settings page you can select what channels you are prompting people to share on. You should limit this to the channels you are active in, so that you can see when people talk about you and join in the converations.</p>
+
+<p>You can also select the types of pages that the buttons appear on. Blog posts are the most common but sometimes you may want to show them on pages, or projects.</p>
