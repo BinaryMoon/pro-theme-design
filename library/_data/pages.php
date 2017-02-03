f21cd36eab88834544258b906ec6b121aa17ac5f
BinaryMoon <binarymoon@gmail.com>
2017-02-03 21:50:03 +0000

    change whitespace :)

--- library/_data/pages.php
+++ library/_data/pages.php
@@ -5,51 +5,51 @@
  */
 function get_pages() {
 
-    // see if there's a valid static page to display
-    $pages = array(
-        'theme-customization' => array(
-            'title' => 'WordPress Theme Customization',
-            'description' => 'Customize your theme with <strong>Codeable.io</strong>.',
-            'view' => 'theme-customizer.php',
-        ),
-        'contact/thanks' => array(
-            'title' => 'Thank You',
-            'description' => 'Thanks for the message!',
-            'view' => 'contact-thanks.php',
-        ),
-        'newsletter' => array(
-            'title' => 'Newsletter',
-            'description' => '<strong>Join the Newsletter.</strong> Keep up to date.',
-            'view' => 'newsletter.php',
-        ),
-        'contact' => array(
-            'title' => 'Contact Us',
-            'description' => 'Keep in touch.',
-            'view' => 'contact.php',
-        ),
-        'sitemap' => array(
-            'title' => 'Sitemap',
-            'description' => 'Find your way around.',
-            'view' => 'sitemap.php',
-        ),
-        'policies' => array(
-            'title' => 'Terms and Conditions',
-            'description' => null,
-            'view' => 'terms-and-conditions.php',
-        ),
-        'refund' => array(
-            'title' => 'Gumroad Refund Request',
-            'description' => null,
-            'view' => 'refund.php',
-        ),
-        'why-us' => array(
-            'title' => 'Why Us',
-            'description' => 'Why buy a WordPress theme from Pro Theme Design?',
-            'view' => 'landing-why-us.php',
-        ),
-    );
+	// see if there's a valid static page to display
+	$pages = array(
+		'theme-customization' => array(
+			'title' => 'WordPress Theme Customization',
+			'description' => 'Customize your theme with <strong>Codeable.io</strong>.',
+			'view' => 'theme-customizer.php',
+		),
+		'contact/thanks' => array(
+			'title' => 'Thank You',
+			'description' => 'Thanks for the message!',
+			'view' => 'contact-thanks.php',
+		),
+		'newsletter' => array(
+			'title' => 'Newsletter',
+			'description' => '<strong>Join the Newsletter.</strong> Keep up to date.',
+			'view' => 'newsletter.php',
+		),
+		'contact' => array(
+			'title' => 'Contact Us',
+			'description' => 'Keep in touch.',
+			'view' => 'contact.php',
+		),
+		'sitemap' => array(
+			'title' => 'Sitemap',
+			'description' => 'Find your way around.',
+			'view' => 'sitemap.php',
+		),
+		'policies' => array(
+			'title' => 'Terms and Conditions',
+			'description' => null,
+			'view' => 'terms-and-conditions.php',
+		),
+		'refund' => array(
+			'title' => 'Refund Request',
+			'description' => null,
+			'view' => 'refund.php',
+		),
+		'why-us' => array(
+			'title' => 'Why Us',
+			'description' => 'Why buy a WordPress theme from Pro Theme Design?',
+			'view' => 'landing-why-us.php',
+		),
+	);
 
-    return $pages;
+	return $pages;
 
 }
 
@@ -62,16 +62,16 @@ function get_pages() {
  */
 function page_exists( $slug ) {
 
-    $pages = get_pages();
+	$pages = get_pages();
 
-    // allow the redirects to work with or without slashes
-    $slug = ltrim( $slug, '/' );
-    $slug = rtrim( $slug, '/' );
+	// allow the redirects to work with or without slashes
+	$slug = ltrim( $slug, '/' );
+	$slug = rtrim( $slug, '/' );
 
-    if ( ! empty( $pages[ $slug ] ) ) {
-        return $pages[ $slug ];
-    }
+	if ( ! empty( $pages[ $slug ] ) ) {
+		return $pages[ $slug ];
+	}
 
-    return false;
+	return false;
 
 }
