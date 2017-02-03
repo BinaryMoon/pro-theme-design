466a3e37d60d4688ca2fb4ea3c0e32e16c68476e
BinaryMoon <binarymoon@gmail.com>
2017-02-03 21:50:39 +0000

    update terms

--- views/terms-and-conditions.php
+++ views/terms-and-conditions.php
@@ -1,67 +1,67 @@
 <?php
-    include( '_partials/head.php' );
+	include( '_partials/head.php' );
 ?>
 
-    <body class="page-terms">
+	<body class="page-terms">
 
-        <?php include( '_partials/header.php' ); ?>
+		<?php include( '_partials/header.php' ); ?>
 
-        <div class="wrapper text">
-            <h1>Terms and Conditions</h1>
+		<div class="wrapper text">
+			<h1>Terms and Conditions</h1>
 
-            <h2>Support</h2>
+			<h2>Support</h2>
 
-            <p>We offer support via the relevant support forums. For WordPress.com please use the <a href="https://premium-themes.forums.wordpress.com">premium themes forum</a>. For self hosted themes you can use our <a href="<?php echo path( 'contact/' ); ?>">contact page</a>.</p>
+			<p>We offer support via the relevant support forums. For WordPress.com please use the <a href="https://premium-themes.forums.wordpress.com">premium themes forum</a>. For self hosted themes you can use our <a href="<?php echo path( 'contact/' ); ?>">contact page</a>.</p>
 
-            <p>The support we offer is restricted to fixing bugs in our themes. Unfortunately we don't help with theme modifications/ customisations or installation issues. For help with customizations we have partnered with Codeable who offer <a href="<?php echo path( 'theme-customization/' ); ?>">customization services for WordPress websites</a>.</p>
+			<p>The support we offer is restricted to fixing bugs in our themes. Unfortunately we don't help with theme modifications/ customisations or installation issues. For help with customizations we have partnered with Codeable who offer <a href="<?php echo path( 'theme-customization/' ); ?>">customization services for WordPress websites</a>.</p>
 
-            <p>We do not offer support for problems with third party plugins/ modifications.</p>
+			<p>We do not offer support for problems with third party plugins or theme modifications.</p>
 
-            <p>We do not offer phone support.</p>
+			<p>We do not offer phone support.</p>
 
-            <p>To help clarify this policy we have put together a support document explaining <a href="<?php echo path( 'documentation/general/support-vs-customization/' ); ?>">the differences between support and customization</a>.</p>
+			<p>To help clarify this policy we have put together a support document explaining <a href="<?php echo path( 'documentation/general/support-vs-customization/' ); ?>">the differences between support and customization</a>.</p>
 
-            <h2>License</h2>
+			<h2>License</h2>
 
-            <p>All of our products are licensed under the same license as WordPress, the GPL – there is more information on the <a href="http://codex.wordpress.org/GPL">GPL license</a> available on the WordPress Codex. Support for Pro Theme Design products will only be given to those who purchase a Pro Theme Design product.</p>
+			<p>All of our products are licensed under the same license as WordPress, the GPL – there is more information on the <a href="http://codex.wordpress.org/GPL">GPL license</a> available on the WordPress Codex. Support for Pro Theme Design products will only be given to those who purchase a Pro Theme Design product.</p>
 
-            <h2>Multiple Sites</h2>
+			<h2>Multiple Sites</h2>
 
-            <p>You are welcome to use our themes on as many sites as you like.</p>
+			<p>You are welcome to use our themes on as many sites as you like.</p>
 
-            <h2>Individual Theme Support</h2>
+			<h2>Individual Theme Support</h2>
 
-            <p>Theme support is offered for the life of the product. If a product is retired from sale then support will continue for a further 6 months. After this support will no longer be given for this product.</p>
+			<p>Theme support is offered for one year from the date of purchase. After this date support will be offered at our discretion.</p>
 
-            <p>Products are considered active if they are available on the site. We reserve the right to retire a product at any time without notice.</p>
+			<h2>Theme Club Support</h2>
 
-            <h2>Theme Club Support</h2>
+			<p>Theme Club support is offered for the duration of the theme club subscription. It will only be offered for themes that are currently active in the theme club. We reserve the right to retire a theme from the theme club at any time without notice.</p>
 
-            <p>Theme Club support is offered for the duration of the theme club subscription. It will only be offered for themes that are currently active in the theme club. We reserve the right to retire a theme from the theme club at any time without notice.</p>
+			<h2>Product Updates</h2>
 
-            <h2>Product Updates</h2>
+			<p>We do not schedule, or guarantee, product updates however we do update products. Updates for minor versions will be free. Occassionally we will release a major update to a product and we reserve the right to charge an upgrade fee at our discretion.</p>
 
-            <p>We do not schedule, or guarantee, product updates however we do update products and will notify customers when we do so. Updates for minor versions will be free. Occassionally we will release a major update to a product and we reserve the right to charge an upgrade fee at our discretion.</p>
+			<p>We publish a list of <a href="<?php echo path( 'tools/changelogs/' ); ?>">all theme changelogs</a>.</p>
 
-            <h2>Personal Information</h2>
+			<h2>Personal Information</h2>
 
-            <p>During the auto update process we may send information such as the website url, WordPress version, and theme version to our servers. This is purely for our internal use. Some, such as the website url, is neccessary for the update to work. The WordPress and PHP versions are needed so that we know what technology our customers use so that we can better serve you.</p>
+			<p>During the auto update process we may send information such as the website url, WordPress version, and theme version to our servers. This is purely for our internal use. Some, such as the website url, is neccessary for the update to work. The WordPress and PHP versions are needed so that we know what technology our customers use so that we can better serve you.</p>
 
-            <p>We do not transfer any personal information such as email addresses. We will never share this information with anyone.</p>
+			<p>We do not transfer any personal information such as email addresses. We will never share this information with anyone.</p>
 
-            <h2>Theme License Keys</h2>
+			<h2>Theme License Keys</h2>
 
-            <p>For Theme Auto Updates to work you will need a valid license. Themes can work without a license, however the Auto Updates will not run.</p>
+			<p>For Theme Auto Updates to work you will need a valid license. Themes can work without a license, however the Auto Updates will not run.</p>
 
-            <p>We reserve the right to stop the use of Theme License Keys at any time without notice.</p>
+			<p>We reserve the right to stop the use of Theme License Keys at any time without notice.</p>
 
-            <p>If we withdraw a Theme License Key then your theme will continue to work, however you will no longer receive theme updates.</p>
+			<p>If we withdraw a Theme License Key then your theme will continue to work, however you will no longer receive theme updates.</p>
 
-            <h2>Transferring themes from WordPress.com</h2>
+			<h2>Transferring themes from WordPress.com</h2>
 
-            <p>Unfortunately themes purchased on WordPress.com are separate products and so are non transferable. This is detailed on the <a href="http://en.support.wordpress.com/themes/premium-themes/">premium theme terms on WordPress.com</a>. If you wish to move from WordPress.com to a self hosted site then you will need to repurchase your theme.</p>
-        </div>
+			<p>Unfortunately themes purchased on WordPress.com are separate products and so are non transferable. This is detailed on the <a href="http://en.support.wordpress.com/themes/premium-themes/">premium theme terms on WordPress.com</a>. If you wish to move from WordPress.com to a self hosted site then you will need to repurchase your theme.</p>
+		</div>
 
 
 <?php
-    include( '_partials/footer.php' );
+	include( '_partials/footer.php' );
