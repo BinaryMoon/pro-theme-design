17b6185dbfd12c2f9b014e146ba3d3b49272cc33
BinaryMoon <binarymoon@gmail.com>
2017-02-22 21:36:45 +0000

    decache

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ if ( 'localhost' === $_SERVER['HTTP_HOST'] ) {
 }
 
 // Constants.
-define( 'DECACHE_CSS', '78' );
+define( 'DECACHE_CSS', '79' );
 define( 'DECACHE_JS', '23' );
 define( 'ENV', $environment );
 define( 'GOOGLE_CSE', '010164663960188762731:aajo_mt5e2a' );
