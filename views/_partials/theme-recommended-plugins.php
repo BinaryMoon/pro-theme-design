b051694f7c88fe72385de987d9faf8c1bf532461
BinaryMoon <binarymoon@gmail.com>
2017-03-28 15:31:21 +0100

    tweak recommended themes code

--- views/_partials/theme-recommended-plugins.php
+++ views/_partials/theme-recommended-plugins.php
@@ -1,6 +1,6 @@
-    <h2>Recommended plugins</h2>
+	<h2>Recommended plugins</h2>
 
-    <ul>
-        <li><strong><?php documentation_link( 'jetpack' ); ?></strong> - we support most of the Jetpack functionality. Infinite scroll, custom logo, photon, and more.</li>
-        <li><strong><?php documentation_link( 'styleguide' ); ?></strong> - customize fonts and colours through the theme customizer.</li>
-    </ul>
+	<ul>
+		<li><strong><?php documentation_link( 'jetpack' ); ?></strong> - we support lots of <a href="<?php echo JETPACK; ?>">Jetpack</a> functionality. Infinite scroll, custom logo, photon, and more.</li>
+		<li><strong><?php documentation_link( 'styleguide' ); ?></strong> - customize fonts and colours through the theme customizer.</li>
+	</ul>
