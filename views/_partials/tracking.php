0015030404815fd7394a8419bd0e4fef5282145e
BinaryMoon <binarymoon@gmail.com>
2017-03-28 15:31:52 +0100

    add hotjar tracking

--- views/_partials/tracking.php
+++ views/_partials/tracking.php
@@ -29,3 +29,16 @@
 <script type="text/javascript">
 	twttr.conversion.trackPid('nvfri', { tw_sale_amount: 0, tw_order_quantity: 0 });
 </script>
+
+
+<!-- Hotjar -->
+<script>
+	(function(h,o,t,j,a,r){
+		h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+		h._hjSettings={hjid:451136,hjsv:5};
+		a=o.getElementsByTagName('head')[0];
+		r=o.createElement('script');r.async=1;
+		r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+		a.appendChild(r);
+	})(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
