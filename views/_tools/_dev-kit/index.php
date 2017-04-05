0a22eb8b6593b70a94f009c330e5bc069668bef3
BinaryMoon <binarymoon@gmail.com>
2017-04-05 22:41:05 +0100

    update dev kit page
    change to current list of tools I use to develop WordPress things

--- views/_tools/_dev-kit/index.php
+++ views/_tools/_dev-kit/index.php
@@ -3,40 +3,37 @@
 
 	<h1>How We Build Pro Theme Design</h1>
 
-	<p class="intro">I have chosen to build Pro Theme Design in the open. I like to use the right tool for the job - and feel WordPress is overkill for this type of site. So I've put together this page to show what we use and why.</p>
+	<p class="intro">I have chosen to build Pro Theme Design in the open. I like to use the right tool for the job - and feel WordPress is overkill for this type of site. So I've put together this page to show what I use and why.</p>
 
 	<h2>Development Software</h2>
 
 	<ul>
-		<li><a href="http://brackets.io/">Brackets</a> - for code editing.</li>
+		<li><a href="https://atom.io/">Atom</a> - for code editing.</li>
 		<li><a href="https://www.sketchapp.com/">Sketch</a> - for UI and design things.</li>
 		<li><a href="http://flyingmeat.com/acorn/">Acorn</a> - for photo editing.</li>
 		<li><a href="https://incident57.com/codekit/">Codekit</a> - for processing LESS files, and linting JS.</li>
+		<li><a href="http://gulpjs.com/">GulpJS</a> - for building and deploying files.</li>
 	</ul>
 
 	<h2>Web Technologies</h2>
 
-	<p class="intro">The site is built using HTML5, and CSS3, and javascript. That's all pretty standard these days though. We also use:</p>
+	<p class="intro">The site is built using HTML5, and CSS3, and javascript. That's all pretty standard these days though. I also use:</p>
 
 	<h3>FlightPHP</h3>
 
-	<p><a href="http://flightphp.com/">FlightPHP</a> is a PHP microframework. Essentially it's a simple system that allows us to add route (page paths) and create different views (templates). The Framework is easy to use and allows me to iterate quickly. We use it instead of WordPress because it removes the data layer, and it allows us to be more flexible with templating and routes.</p>
+	<p><a href="http://flightphp.com/">FlightPHP</a> is a PHP microframework. Essentially it's a simple system that allows us to add routes (page paths) and create different views (templates). The Framework is easy to use and allows me to iterate quickly. We use it instead of WordPress because it removes the data layer, and it allows me to be more flexible with templating and routes.</p>
 
 	<h3>JQuery</h3>
 
-	<p>All web developers know about <a href="http://jquery.com">JQuery</a>, it's a javascript library that makes things a lot easier. We've considered going framework free, or using a slimmer framework like Zepto, however the convenience and flexibility of JQuery is worth more than the effort required to switch.</p>
-
-	<h3>Font Awesome</h3>
-
-	<p><a href="http://fortawesome.github.io/Font-Awesome/">Font Awesome</a> is a massive, comprehensive, icon font library. We've used them librally around the site. They are a quick and easy way to make the site look more interesting - without adding a huge overhead.</p>
+	<p>All web developers know about <a href="http://jquery.com">JQuery</a>, it's a javascript library that makes things a lot easier. I've considered going framework free, or using a slimmer framework like Zepto, however the convenience and flexibility of JQuery is worth more than the effort required to switch.</p>
 
 	<h3>Google Fonts</h3>
 
-	<p><a href="https://fonts.google.com/">Google Fonts</a> are awesome. We use <a href="https://fonts.google.com/specimen/Lato?query=lato">Lato</a>.</p>
+	<p><a href="https://fonts.google.com/">Google Fonts</a> are awesome. I use <a href="https://fonts.google.com/specimen/Poppins">Poppins</a>.</p>
 
 	<h2>Third Party Services</h2>
 
-	<p class="intro">There seems to be an obsession in the WordPress world of doing everything yourself. We feel this is more often than not a poor choice. You can probably find a plugin that does everything you want, however these will often not be as good as a third party service that is constantly developed and maintained.</p>
+	<p class="intro">There seems to be an obsession in the WordPress world of doing everything with WordPress. I feel this can sometimes be a poor choice. You can probably find a plugin that does whateveer you want, however these may not be as good as a third party service that is constantly developed and maintained.</p>
 
 	<h3>Contact Forms - Formspree</h3>
 
@@ -48,11 +45,11 @@
 
 	<p><a href="http://mailchimp.com">Mailchimp</a> is a great service for newsletters. We love their flexibility and wealth of features.</p>
 
-	<h3>Online Store - Gumroad (and Creative Market)</h3>
+	<h3>Online Store - Fastspring (and Creative Market)</h3>
 
 	<p>Many people use WooCommerce or Easy Digital Downloads for selling things online, but these can be complex to setup and maintain. Using a third party service designed for selling products removes all maintanence, and security concerns. If you chose the right services then it also removes VATMOSS requirements (particularly important for people in the EU).</p>
 
-	<p>We sell our products through 2 services. <a href="http://gumroad.com">GumRoad</a>, and <a href="https://creativemarket.com/BinaryMoon?u=BinaryMoon">Creative Market</a>.</p>
+	<p>We sell our products through 2 services. <a href="http://fastspring.com">Fastspring</a>, and <a href="https://creativemarket.com/BinaryMoon?u=BinaryMoon">Creative Market</a>.</p>
 
 	<h3>Analytics - Google Analytics</h3>
 
@@ -62,7 +59,17 @@
 
 	<p><a href="https://github.com/binarymoon/pro-theme-design">Pro Theme Design is stored in a public Github repository</a>. This repository includes everything. For example this is a flat site - there's no database. We store data in arrays, that reference flat text/ html files that store the actual content.</p>
 
-	<p class="note icon"><?php draw_svg( 'alert-info', 'large' ); ?>We'd be happy to receive pull requests and issues.</p>
+	<p>We also host <a href="https://github.com/binarymoon/granule">Granule</a>, our WordPress starter theme, on Github.</p>
+
+	<p class="note icon"><?php draw_svg( 'alert-info', 'large' ); ?>We'd be happy to receive pull requests and issues for any of our projects.</p>
+
+	<h3>Version Control - Bitbucket</h3>
+
+	<p>For private code (our themes) we use the free <a href="https://bitbucket.com">Bitbucket</a> account. <a href="https://www.sourcetreeapp.com/">Sourcetree</a> is also a great, free, desktop git  client.</p>
+
+	<h3>Accounts - FreeAgent</h3>
+
+	<p>Doing your accounts is a neccessary part of running a small business. <a href="http://fre.ag/43bm4hft">FreeAgent</a> takes a lot of the pain out of it so that I can put in the minimum effort possible. Love it.</p>
 
 	<h3>Site Search - Google Custom Search Engine</h3>
 
