d0adda0676be62163f2f1eb03520ea05f0a4f168
BinaryMoon <binarymoon@gmail.com>
2017-04-24 20:56:06 +0100

    add info to Romero theme page

--- views/_themes/_details/romero.php
+++ views/_themes/_details/romero.php
@@ -3,3 +3,14 @@
 	<p class="intro"><strong>Romero</strong> is designed for magazine sites, highlighting full-width featured images that help your main blog posts really stand out. It’s an ideal theme for video game sites, car magazines, and other topics well suited to large, vibrant imagery.</p>
 
 </div>
+
+<div class="wrapper features">
+
+<?php
+	site_include_view( '_themes/_features/social.php' );
+	site_include_view( '_themes/_features/code-review.php' );
+	site_include_view( '_themes/_features/responsive.php' );
+	site_include_view( '_themes/_features/translations.php' );
+?>
+
+</div>
