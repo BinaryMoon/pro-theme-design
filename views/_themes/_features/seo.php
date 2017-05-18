7047a51c63ebd9d4d5ed277a7e0c406891a1a932
alexdenning <alexdenning@gmail.com>
2017-05-18 16:58:12 +0100

    Update seo.php
    Updated copy.

--- views/_themes/_features/seo.php
+++ views/_themes/_features/seo.php
@@ -2,9 +2,9 @@
 	<section>
 		<div class="content">
 			<h3 class="number">
-				Search Engine Optimised
+				Search Engine Optimized
 			</h3>
-			<p class="description">A beautiful structure that adapts to your content with a responsive layout that works on any device.</p>
+			<p class="description">Search engines will love your site running this theme! We adhere to the very highest coding standards, so search engines will be able to easily find your content and display it prominently in their results.</p>
 		</div>
 	</section>
 	<header>
