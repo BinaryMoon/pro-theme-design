f0149a3cc19a70c815f58c553afda1bccea8c0b2
alexdenning <alexdenning@gmail.com>
2017-05-18 16:45:18 +0100

    Update code-review.php
    Update copy.

--- views/_themes/_features/code-review.php
+++ views/_themes/_features/code-review.php
@@ -2,9 +2,9 @@
 	<section>
 		<div class="content">
 			<h3 class="number">
-				Code Reviewed
+				Expertly Reviewed Code
 			</h3>
-			<p class="description">Reviewed by the team at wordpress.com - this means a solid, reliable, and secure website.</p>
+			<p class="description">This theme's code has been reviewed line-by-line by the WordPress.com developer team, meaning you can be doubly confident the code is solid, reliable and secure – and your website will be too.</p>
 		</div>
 	</section>
 	<header>
