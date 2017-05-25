06902b157622f0edca39d10e531f7d5dc1853693
alexdenning <alexdenning@gmail.com>
2017-05-25 10:27:48 +0100

    Update buddypress.php
    Added BuddyPress copy

--- views/_themes/_features/buddypress.php
+++ views/_themes/_features/buddypress.php
@@ -3,8 +3,8 @@
 		<div class="content">
 			<h3 class="number">
 				Social Network
-			</h3>
-			<p class="description">Social networking and community building info.</p>
+			</h3>			
+			<p class="description">This theme lets you build an advanced social network on your website using WordPress' BuddyPress plugin. Let your users add profiles, send messages and join your community, all seamlessly.</p>
 		</div>
 	</section>
 	<header>
