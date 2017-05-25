ef4c40ab5cb6ae8e06307e3ea3272c90d17b546a
BinaryMoon <binarymoon@gmail.com>
2017-05-25 09:54:39 +0100

    add stubs for new plugins

---
+++ views/_themes/_features/bbpress.php
@@ -0,0 +1,13 @@
+<div class="wrapper block feature big">
+	<section>
+		<div class="content">
+			<h3 class="number">
+				Integrated Forum
+			</h3>
+			<p class="description">Forum and community.</p>
+		</div>
+	</section>
+	<header>
+		<img src="<?php echo image_path( 'theme-features/code-review.png' ); ?>" />
+	</header>
+</div>
---
+++ views/_themes/_features/buddypress.php
@@ -0,0 +1,13 @@
+<div class="wrapper block feature big">
+	<section>
+		<div class="content">
+			<h3 class="number">
+				Social Network
+			</h3>
+			<p class="description">Social networking and community building info.</p>
+		</div>
+	</section>
+	<header>
+		<img src="<?php echo image_path( 'theme-features/code-review.png' ); ?>" />
+	</header>
+</div>
