d49609d2620b54329d11b89c65fe3beb5ac5663c
BinaryMoon <binarymoon@gmail.com>
2017-05-27 13:29:55 +0100

    stub for events calendar plugin

---
+++ views/_themes/_features/events-calendar.php
@@ -0,0 +1,13 @@
+<div class="wrapper block feature big">
+	<section>
+		<div class="content">
+			<h3 class="number">
+				Events Calendar
+			</h3>
+			<p class="description"></p>
+		</div>
+	</section>
+	<header>
+		<img src="<?php echo image_path( 'theme-features/code-review.png' ); ?>" />
+	</header>
+</div>
