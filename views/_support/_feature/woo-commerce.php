7d92399c72bf3276cbbffa48fbcb3eeef8965081
BinaryMoon <binarymoon@gmail.com>
2017-06-23 14:24:43 +0100

    Update WooCommerce docs

--- views/_support/_feature/woo-commerce.php
+++ views/_support/_feature/woo-commerce.php
@@ -4,6 +4,12 @@
 
 <p>You can <a href="https://wordpress.org/plugins/woocommerce/">download WooCommerce</a> from the WordPress plugin repository, or directly inside your site admin by searching for WooCommerce.</p>
 
+<p>Once activated you will be given prompts to set up your store and then be given access to add products to your site.</p>
+
+<p>If you are developing locally then they have some <a href="https://docs.woocommerce.com/document/importing-woocommerce-dummy-data/">WooCommerce demo content</a> you can use as a starting point.</p>
+
 <h2>Getting help with WooCommerce</h2>
 
-<p>Unfortunately we don't offer customer support for WooCommerce. Since it's such a big plugin there's too much there for us to help with, however they have extensive documentation and a dedicated support team for you to get help from.</p>
+<p>For new users, WooCommerce has a complete <a href="https://docs.woocommerce.com/documentation/plugins/woocommerce/getting-started/">Getting Started guide</a>.</p>
+
+<p>Unfortunately we don't offer customer support for WooCommerce. Since it's such a big plugin there's too much there for us to help with, however they have <a href="https://docs.woocommerce.com/">extensive documentation</a> and a dedicated support team for you to get help from.</p>
