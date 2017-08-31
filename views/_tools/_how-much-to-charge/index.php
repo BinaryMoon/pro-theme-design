946bb5db028f810adb92716fabfd39f45545e452
BinaryMoon <binarymoon@gmail.com>
2017-08-31 20:20:32 +0100

    update how much to charge codes
    Add an expenses field to cover other costs beyond hourly rates

--- views/_tools/_how-much-to-charge/index.php
+++ views/_tools/_how-much-to-charge/index.php
@@ -1,91 +1,109 @@
 <?php
-    site_script( path( 'views/_tools/_how-much-to-charge/scripts.min.js' ) );
+	site_script( path( 'views/_tools/_how-much-to-charge/scripts.min.js' ) );
 ?>
 <div class="wrapper text">
-    <?php site_breadcrumbs(); ?>
+	<?php site_breadcrumbs(); ?>
 
-    <h1>WordPress Freelance Pricing Calculator</h1>
+	<h1>WordPress Freelance Pricing Calculator</h1>
 
-    <form class="entity-calculator">
-        <label>
-            <span>Your hourly rate?</span>
-            <select id="currency" data-size="short">
-                <option value="$">$</option>
-                <option value="£">£</option>
-                <option value="€">€</option>
-                <option value="¥">¥</option>
-            </select>
-            <input type="number" step="10" min="10" id="rate" value="150" />
-            <p class="hint">You can <a href="http://motivapp.com/freelance-hourly-rate-calculator" target="_blank">calculate your rate here</a></p>
-        </label>
+	<form class="entity-calculator">
 
-        <label>
-            <span>Approximate how long will the project take to complete?</span>
-            <input type="number" step="1" min="1" id="duration" value="20" />
-            <p class="hint">Make sure to include time for everything - emails, calls, meetings etc</p>
-        </label>
+		<label class="hourly-rate group">
+			<span>Your hourly rate?</span>
 
-        <label>
-            <span>Who?</span>
-        </label>
-        <div class="radio-group">
-            <label>
-                Charity
-                <input type="radio" name="client" value="0" />
-            </label>
-            <label>
-                Individual
-                <input type="radio" name="client" value="0.2" />
-            </label>
-            <label class="checked">
-                Small/ Medium Business
-                <input type="radio" name="client" value="0.4" checked />
-            </label>
-            <label>
-                Large Company
-                <input type="radio" name="client" value="0.6" />
-            </label>
-        </div>
+			<div class="row">
 
-        <label>
-            <span>Interest in the project?</span>
-        </label>
-        <div class="radio-group">
-            <label>
-                No Interest
-                <input type="radio" name="interest" value="0.6" checked />
-            </label>
-            <label class="checked">
-                Could be fun
-                <input type="radio" name="interest" value="0.4" checked />
-            </label>
-            <label>
-                Good learning excercise
-                <input type="radio" name="interest" value="0.2" />
-            </label>
-            <label>
-                Dream Project
-                <input type="radio" name="interest" value="0" />
-            </label>
-        </div>
+				<select id="currency" data-size="short">
+					<option value="$">$</option>
+					<option value="£">£</option>
+					<option value="€">€</option>
+					<option value="¥">¥</option>
+				</select>
 
-        <div class="note message">
-            <p class="quote">You should quote: <span>?</span></p>
-            <p class="quote-addendum">With a base price (break even) of: <span>?</span></p>
-        </div>
+				<input type="number" step="10" min="10" id="rate" value="150" class="hourly-value"/>
 
-    </form>
+			</div>
+
+			<p class="hint">You can <a href="http://motivapp.com/freelance-hourly-rate-calculator" target="_blank">calculate your rate here</a></p>
+
+		</label>
+
+		<label class="group">
+			<span>Approximate how many hours will the project take to complete?</span>
+			<input type="number" step="1" min="1" id="duration" value="20" />
+			<p class="hint">Make sure to include time for everything - emails, calls, meetings etc</p>
+		</label>
+
+		<label class="group group-expenses">
+			<span>What are your expenses?</span>
+			<span class="symbol">$</span>
+			<input type="number" step="1" min="1" id="expenses" value="0" />
+			<p class="hint">For example any software/ <a href="<?php echo path( '/themes/' ); ?>">WordPress themes</a>/ plugins you need to buy, contractor costs, travel for meetings etc.</p>
+		</label>
+
+		<label>
+			<span>Who is the project for?</span>
+		</label>
+
+		<div class="radio-group">
+			<label>
+				Charity
+				<input type="radio" name="client" value="0" />
+			</label>
+			<label>
+				Individual
+				<input type="radio" name="client" value="0.2" />
+			</label>
+			<label class="checked">
+				Small/ Medium Business
+				<input type="radio" name="client" value="0.4" checked />
+			</label>
+			<label>
+				Large Company
+				<input type="radio" name="client" value="0.6" />
+			</label>
+		</div>
+
+		<label>
+			<span>Your Interest in the project?</span>
+		</label>
+
+		<div class="radio-group">
+			<label>
+				No Interest
+				<input type="radio" name="interest" value="0.6" checked />
+			</label>
+			<label class="checked">
+				Could be fun
+				<input type="radio" name="interest" value="0.4" checked />
+			</label>
+			<label>
+				Good learning excercise
+				<input type="radio" name="interest" value="0.2" />
+			</label>
+			<label>
+				Dream Project
+				<input type="radio" name="interest" value="0" />
+			</label>
+		</div>
+
+		<div class="note message">
+			<p class="quote">You should quote: <span>?</span></p>
+			<p class="quote-addendum">With a base price (break even) of: <span>?</span></p>
+		</div>
+
+	</form>
 
 
-    <h2>The Theory</h2>
-    <p>The base price is calculated by multiplying your hourly rate by your time estimate. We then add some extra time for contingency in case of unexpected problems. Finally we round the base price, to make it look nicer.</p>
-    <p>Next we look at how important the job and client are to you and adjust the rate accordingly.</p>
-    <p>This gives you a base rate - and an ideal rate. You should <strong>quote the ideal rate</strong> - knowing that you can drop to the base rate and still get a fair deal. Of course, you should always stick to the ideal rate if you can!</p>
-    <p>Read more on how to <a href="<?php echo path( 'how-to/earn-more-as-a-wordpress-freelancer/' ); ?>">charge more as a WordPress freelancer</a>.</p>
+	<h2>The Theory</h2>
+	<p>The base price is calculated by multiplying your hourly rate by your time estimate. We then add some extra time for contingency in case of unexpected problems. Finally we round the base price, to make it look nicer.</p>
+	<p>Next we look at how important the job and client are to you and adjust the rate accordingly.</p>
+	<p>This gives you a base rate - and an ideal rate. You should <strong>quote the ideal rate</strong> - knowing that you can drop to the base rate and still get a fair deal. Of course, you should always stick to the ideal rate if you can!</p>
+	<p>Read more on how to <a href="<?php echo path( 'how-to/earn-more-as-a-wordpress-freelancer/' ); ?>">charge more as a WordPress freelancer</a>.</p>
 
 
 
 </div>
 
 <?php
-    site_include_view( '_partials/more-themes.php' );
+	site_include_view( '_partials/more-themes.php' );
--- views/_tools/_how-much-to-charge/scripts.js
+++ views/_tools/_how-much-to-charge/scripts.js
@@ -2,59 +2,67 @@
 
 (function( $ ) {
 
-    $( document ).ready( function() {
+	$( document ).ready( function() {
 
-        $( 'form input, form select' ).on( 'change', function( e ) {
+		$( 'form input, form select' ).on(
+			'input change',
+			function( e ) {
 
-            e.stopPropagation();
-            e.preventDefault();
+				e.stopPropagation();
+				e.preventDefault();
 
-            calculate();
+				calculate();
 
-        } );
+			}
+		);
 
-    } );
+	} );
 
-    // calculate the quote
-    function calculate() {
+	// calculate the quote
+	function calculate() {
 
+		// the amount you should quote
+		var quote = '';
 
-        // the amount you should quote
-        var quote = '';
+		// the base price for the job
+		var quote_addendum = '';
 
-        // the base price for the job
-        var quote_addendum = '';
+		// contingency money in case of problems
+		var contingency = 1.5;
 
-        // contingency money in case of problems
-        var contingency = 1.5;
+		// how much effort is the project for you to work on. How much does it benefit you?
+		// the higher the value, the more effort the project will be so the more you should earn from it
+		var effort = 1;
 
-        // how much effort is the project for you to work on. How much does it benefit you?
-        // the higher the value, the more effort the project will be so the more you should earn from it
-        var effort = 1;
+		var currency = $( '#currency' ).val();
+		var rate = parseFloat( $( '#rate' ).val() );
+		var duration = parseFloat( $( '#duration' ).val() );
+		var expenses = parseFloat( $( '#expenses' ).val() );
+		var client = parseFloat( $( 'input[type=radio][name=client]:checked' ).val() );
+		var interest = parseFloat( $( 'input[type=radio][name=interest]:checked' ).val() );
 
-        var currency = $( '#currency' ).val();
-        var rate = parseFloat( $( '#rate' ).val() );
-        var duration = parseFloat( $( '#duration' ).val() );
-        var client = parseFloat( $( 'input[type=radio][name=client]:checked' ).val() );
-        var interest = parseFloat( $( 'input[type=radio][name=interest]:checked' ).val() );
+		// Change currency symbol.
+		$( 'label .symbol' ).html( currency );
 
-        effort = effort + client + interest;
+		effort = effort + client + interest;
 
-        // base rate
-        quote_addendum = rate * duration * contingency;
-        // round value
-        quote_addendum = Math.ceil( quote_addendum / 10 ) * 10;
+		// base rate
+		quote_addendum = ( rate * duration * contingency ) + expenses;
+		// round value.
+		quote_addendum = Math.ceil( quote_addendum );
 
-        quote = quote_addendum * effort;
-        // round value
-        quote = Math.ceil( quote / 100 ) * 100;
+		// total quote
+		quote = quote_addendum * effort;
+		// round value to nearest hundred
+		quote = Math.ceil( quote / 100 ) * 100;
+		quote = quote.toLocaleString();
 
-        // assign the values
-        $( 'p.quote span' ).html( currency + quote );
-        $( 'p.quote-addendum span' ).html( currency + quote_addendum );
+		// assign the values
+		$( 'p.quote span' ).html( currency + quote );
+		$( 'p.quote-addendum span' ).html( currency + quote_addendum );
 
-    }
+	}
 
-    calculate();
+	calculate();
 
 } )( jQuery );
