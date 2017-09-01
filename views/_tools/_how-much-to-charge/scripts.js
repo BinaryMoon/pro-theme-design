f739c20a07021541e17ea141194bb119c9e6b5ff
BinaryMoon <binarymoon@gmail.com>
2017-09-01 07:55:46 +0100

    fix float values so that null values don't cause a nan error

--- views/_tools/_how-much-to-charge/scripts.js
+++ views/_tools/_how-much-to-charge/scripts.js
@@ -35,11 +35,11 @@
 		var effort = 1;
 
 		var currency = $( '#currency' ).val();
-		var rate = parseFloat( $( '#rate' ).val() );
-		var duration = parseFloat( $( '#duration' ).val() );
-		var expenses = parseFloat( $( '#expenses' ).val() );
-		var client = parseFloat( $( 'input[type=radio][name=client]:checked' ).val() );
-		var interest = parseFloat( $( 'input[type=radio][name=interest]:checked' ).val() );
+		var rate = getFloat( '#rate', 10 );
+		var duration = getFloat( '#duration', 1 );
+		var expenses = getFloat( '#expenses', 0 );
+		var client = getFloat( 'input[type=radio][name=client]:checked', 0.6 );
+		var interest = getFloat( 'input[type=radio][name=interest]:checked', 0.6 );
 
 		// Change currency symbol.
 		$( 'label .symbol' ).html( currency );
@@ -60,6 +60,23 @@
 		// assign the values
 		$( 'p.quote span' ).html( currency + quote );
 		$( 'p.quote-addendum span' ).html( currency + quote_addendum );
+
+	}
+
+	/**
+	 * Get a float value from an element, with a default value in case it's removed.
+	 */
+	function getFloat( element, default_value ) {
+
+		var number = parseFloat( $( element ).val() );
+
+		if ( isNaN( number ) ) {
+			number = default_value;
+		}
+
+		$( element ).val( number );
+
+		return number;
 
 	}
 
