d3d8208670eb66ec207d0b0c0db44c415f66598e
BinaryMoon <binarymoon@gmail.com>
2018-01-14 14:32:24 +0000

    tidy code formatting

--- views/_tools/_developer-resources/index.php
+++ views/_tools/_developer-resources/index.php
@@ -1,24 +1,30 @@
 <?php
-	$resources = array(
-		'' => '',
-		'calltoidea.com' => 'Interface Inspiration',
-		'cssreference.io' => 'CSS Reference',
-		'place.to' => 'Insert screenshots into different computing situations.',
-		'frontendhandbook.com' => 'Documentation and reference for front end developers.',
-		'facebook.github.io/design/devices.html' => 'Sketch files of popular devices.',
-		'mapglyphs.com/world-maps' => 'World Map images.',
-		'hemingwayapp.com' => 'An online text proof reader.',
-		'brandcolors.net' => 'Website brand colours, all in one place.',
-		'cssfontstack.com' => 'A comprehensive set of CSS font stacks.',
-		'browserstack.com' => 'An online browser testing tool.',
-		'bennettfeely.com/clippy' => 'A CSS clipping path generator.',
-		'subtlepatterns.com' => 'A collection of images that are great for website/ presentation backgrounds.',
-		'colorzilla.com/gradient-editor' => 'CSS Gradient Generator.',
-		'svgporn.com' => 'A huge collection of SVG logos and icons for various companies.',
-		'fortawesome.github.io/Font-Awesome' => 'Comprehensive icon font collection.',
-		'htaccess.madewithlove.be' => 'HTAccess Tester.',
+/**
+ * Display a list of resources.
+ *
+ * @package Ptd
+ */
 
-	);
+$resources = array(
+	'' => '',
+	'calltoidea.com' => 'Interface Inspiration',
+	'cssreference.io' => 'CSS Reference',
+	'place.to' => 'Insert screenshots into different computing situations.',
+	'frontendhandbook.com' => 'Documentation and reference for front end developers.',
+	'facebook.github.io/design/devices.html' => 'Sketch files of popular devices.',
+	'mapglyphs.com/world-maps' => 'World Map images.',
+	'hemingwayapp.com' => 'An online text proof reader.',
+	'brandcolors.net' => 'Website brand colours, all in one place.',
+	'cssfontstack.com' => 'A comprehensive set of CSS font stacks.',
+	'browserstack.com' => 'An online browser testing tool.',
+	'bennettfeely.com/clippy' => 'A CSS clipping path generator.',
+	'subtlepatterns.com' => 'A collection of images that are great for website/ presentation backgrounds.',
+	'colorzilla.com/gradient-editor' => 'CSS Gradient Generator.',
+	'svgporn.com' => 'A huge collection of SVG logos and icons for various companies.',
+	'fortawesome.github.io/Font-Awesome' => 'Comprehensive icon font collection.',
+	'htaccess.madewithlove.be' => 'HTAccess Tester.',
+);
+
 ?>
 
 <div class="wrapper text">
