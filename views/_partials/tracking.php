59d38e95428567f2455238ddb03c107066bc0e24
BinaryMoon <binarymoon@gmail.com>
2018-05-26 21:18:06 +0100

    tweak facebook tracking, and remove hotjar, and twitter since I don't use them

--- views/_partials/tracking.php
+++ views/_partials/tracking.php
@@ -4,7 +4,7 @@
 	n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
 	n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
 	t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-	document,'script','//connect.facebook.net/en_US/fbevents.js');
+	document,'script','https://connect.facebook.net/en_US/fbevents.js');
 
 	fbq('init', '605404886265234');
 	fbq('track', 'PageView');
@@ -16,29 +16,9 @@
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 	ga( 'create', 'UA-55722-9', 'auto' );
 	ga( 'require', 'GTM-PDLL8GK' );
 	ga( 'send', 'pageview' );
-</script>
-
-
-<!-- Twitter single-event website tag code -->
-<script src="//platform.twitter.com/oct.js" type="text/javascript"></script>
-<script type="text/javascript">
-	twttr.conversion.trackPid('nvfri', { tw_sale_amount: 0, tw_order_quantity: 0 });
-</script>
-
-
-<!-- Hotjar -->
-<script>
-	(function(h,o,t,j,a,r){
-		h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-		h._hjSettings={hjid:451136,hjsv:5};
-		a=o.getElementsByTagName('head')[0];
-		r=o.createElement('script');r.async=1;
-		r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-		a.appendChild(r);
-	})(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
 </script>
