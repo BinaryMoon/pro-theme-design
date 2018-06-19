3de00c71e0ffb57dc51849e8938988a7f0139abc
BinaryMoon <binarymoon@gmail.com>
2018-06-19 12:18:59 +0100

    Update Traveler theme docs

---
+++ views/_support/_theme/traveler.php
@@ -0,0 +1,18 @@
+<h2>Customizer Settings</h2>
+
+<ul>
+	<li><strong>Display Category Navigation</strong> - toggle the default category navigation that appears under the header.</li>
+	<li><strong>Header Height</strong> - change the height of the header image.</li>
+</ul>
+
+<h2>Menus</h2>
+
+<p>Traveler has 2 menus available. By default the top menu displays your websites pages and the lower menu displays your websites categories.</p>
+
+<p>You can customize the top menu using the WordPress custom menu functionality found in the Customizer.</p>
+
+<p>The lower menu is always set to display categories and will display the top 12 categories, sorted by the number of posts in each category. You can hide the category menu in the theme settings found in the Customizer. You can also replace the category menu with a custom menu in the menu section of the Customizer.</p>
+
+<h2>Sidebars</h2>
+
+<p>Traveler has a single optional sidebar. This will display in the footer of the site and uses javascript to intelligently position the widgets to make sure they slot together nicely.</p>
