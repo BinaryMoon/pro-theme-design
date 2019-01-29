4b0e8c1cfcd3fff2018725f2670cfa256717da53
BinaryMoon <binarymoon@gmail.com>
2019-01-29 18:21:58 +0000

    Remove redundant alt text

--- views/_partials/footer.php
+++ views/_partials/footer.php
@@ -11,7 +11,7 @@
 
 				<div class="blurb blurb-ben">
 
-					<img id="ava-ben" class="avatar" src="<?php echo image_path( 'website/ava-ben.jpg' ); ?>" alt="Ben Gillbanks">
+					<img id="ava-ben" class="avatar" src="<?php echo image_path( 'website/ava-ben.jpg' ); ?>" alt="">
 					<h5>Ben Gillbanks</h5>
 					<p class="location">Devon, England</p>
 					<p class="bio">Ben is a full stack developer - doing both design and development - often turning Darrens ideas into real products, but not averse to designing his own themes and writing the occassional plugin. Plus he blogs about WordPress and tech at <a href="https://www.binarymoon.co.uk/">Binary Moon</a> and runs <a href="https://masterwp.co">MasterWP - WordPress newsletter</a>.</p>
@@ -35,7 +35,7 @@
 
 				<div class="blurb blurb-darren">
 
-					<img id="ava-darren" class="pos avatar" src="<?php echo image_path( 'website/ava-darren.jpg' ); ?>" alt="Darren Hoyt">
+					<img id="ava-darren" class="pos avatar" src="<?php echo image_path( 'website/ava-darren.jpg' ); ?>" alt="">
 					<h5>Darren Hoyt</h5>
 					<p class="location">Brooklyn, NY</p>
 					<p class="bio">Established in the WordPress community for projects like <strong>Mimbo</strong> and <strong>Agregado</strong>, Darren also has 14 years experience designing websites for businesses and startups. His role at Pro Theme Design is taking what Ben builds and making it beautiful and simple to use.</p>
