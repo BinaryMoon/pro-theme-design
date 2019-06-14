43baa0a74b974bf0138f18f3ec5fe0dc8edf5b73
Ben Gillbanks <binarymoon@gmail.com>
2019-06-14 08:58:39 +0100

    add a curl function
    for use with the new geoip database

--- library/functions.php
+++ library/functions.php
@@ -346,3 +346,27 @@ function slugify( $string ) {
 	return strtolower( trim( $string ) );
 
 }
+
+
+/**
+ * A replacement for file_get_contents that uses Curl.
+ *
+ * @param string $url The filepath to get.
+ * @return string
+ */
+function url_get_contents ( $url ) {
+
+	if ( ! function_exists( 'curl_init' ) ) {
+		die( 'CURL is not installed!' );
+	}
+
+	$ch = curl_init();
+	curl_setopt( $ch, CURLOPT_URL, $url );
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+	$output = curl_exec( $ch );
+
+	curl_close( $ch );
+
+	return $output;
+
+}
