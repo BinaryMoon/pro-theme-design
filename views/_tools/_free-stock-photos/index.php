43eccc9945a1f0089a5377eafcb19c5e78f678e4
Ben Gillbanks <binarymoon@gmail.com>
2019-08-28 23:08:57 +0100

    Update stock photo collection

--- views/_tools/_free-stock-photos/index.php
+++ views/_tools/_free-stock-photos/index.php
@@ -5,6 +5,7 @@
 		'scatterjar.com' => 'Scatter Jar, food photos',
 		'gratisography.com' => 'Free high-res pictures by Ryan McGuire (GPL compat)',
 		'picjumbo.com' => 'Jumbo Photos',
+		'images.nasa.gov' => 'NASA Images',
 		'photopin.com' => 'Photo Pin',
 		'pixabay.com' => 'Pixabay (GPL compat)',
 		'flickr.com/photos/wocintechchat/' => 'Women of Color in Tech',
@@ -13,7 +14,6 @@
 		'oldbookillustrations.com' => 'Old Book Illustrations',
 		'mystock.photos' => 'My Stock Photos',
 		'flickr.com/photos/britishlibrary/' => 'British Library (GPL compat)',
-		'viintage.com' => 'Viintage (GPL compat)',
 		'burst.shopify.com' => 'Burst from Shopify',
 		'skuawk.com' => 'Skuawk CC0 photography',
 		'nos.twnsnd.co' => 'New Old Stock (GPL compat)',
@@ -21,7 +21,6 @@
 		'jaymantri.com' => 'Jay Mantri',
 		'visualhunt.com' => 'Visual Hunt',
 		'freeforcommercialuse.net' => 'Free For Commercial Use',
-		'publicdomainarchive.com' => 'Public Domain Archive (GPL compat)',
 		'www.publicdomainpictures.net' => 'Public Domain Pictures (GPL compat)',
 		'en.freejpg.com.ar' => 'FreeJPG',
 		'magdeleine.co/browse' => 'Hand picked photos',
@@ -41,17 +40,13 @@
 		'moveast.me' => 'Move East',
 		'barnimages.com' => 'Barn Images',
 		'freepik.com' => 'Free Pik, photos and vectors',
-		'peek.space' => 'Space Photos',
 		'freenaturestock.com' => 'Free Nature Stock',
 		'photocollections.io' => 'Photo Collections',
 		'streetwill.co' => 'Street Will',
 		'jeshoots.com' => 'Photos by Jan Vasek',
 		'lifeofpix.com' => 'Life of Pix',
 		'negativespace.co' => 'Negative Space',
-		'freeuse.io' => 'Free Use',
 		'splitshire.com' => 'Split Shire (GPL compat)',
-		'lockandstockphotos.com' => 'Lock and Stock Photos',
-		'epicva.com' => 'Epic VA',
 	);
 ?>
 
@@ -59,7 +54,7 @@
 	<?php site_breadcrumbs(); ?>
 
 	<h1>Free Stock Photos</h1>
-	<p class="intro">This is a collection of the finest <strong>FREE</strong> stock photography sites online, over <strong><?php echo count( $stock_sites ); ?></strong> of them!. Make sure to check the individual websites for photo license information. All sites have free photos, but some require some kind of credit.</p>
+	<p class="intro">This is a collection of the finest <strong>FREE</strong> stock photography sites online, over <strong><?php echo count( $stock_sites ); ?></strong> of them! Make sure to check the individual websites for photo license information. All sites have free photos, but some require some kind of credit.</p>
 	<p>We've labeled the resoures that are compatible with the GPL - which is the license WordPress uses. If you want something more unique then you can get some <a href="https://creativemarket.com/photos?u=BinaryMoon">fantastic stock photos at Creative Market</a>.</p>
 </div>
 
