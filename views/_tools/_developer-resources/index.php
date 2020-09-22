7561f6c31683bbc4f00674bca20b3f1cbd4d1ff0
Ben Gillbanks <binarymoon@gmail.com>
2020-09-22 09:41:14 +0100

    Update tools page content, and layout

--- views/_tools/_developer-resources/index.php
+++ views/_tools/_developer-resources/index.php
@@ -6,25 +6,113 @@
  */
 
 $resources = array(
-	'www.remove.bg' => 'Remove Backgrounds from Images',
-	'uifaces.co' => 'User Interface Faces',
-	'calltoidea.com' => 'Interface Inspiration',
-	'cssreference.io' => 'CSS Reference',
-	'ethical.net' => 'Ethical apps and services',
-	'postcodes.io' => 'UK Postcodes API',
-	'mockup.photos' => 'Insert screenshots into different computing situations.',
-	'mapglyphs.com/world-maps' => 'World Map images.',
-	'hemingwayapp.com' => 'An online text proof reader.',
-	'devdocs.io' => 'API docs for everything web.',
-	'brandcolors.net' => 'Website brand colours, all in one place.',
-	'cssfontstack.com' => 'A comprehensive set of CSS font stacks.',
-	'audiomass.co' => 'Browser based audio editor (like Audacity).',
-	'www.lambdatest.com' => 'An online browser testing tool.',
-	'bennettfeely.com/clippy' => 'A CSS clipping path generator.',
-	'subtlepatterns.com' => 'A collection of images that are great for website/ presentation backgrounds.',
-	'svgporn.com' => 'A huge collection of SVG logos and icons for various companies.',
-	'htaccess.madewithlove.be' => 'HTAccess Tester.',
-	'www.screely.com' => 'Make screenshots look nice quickly.',
+
+	array(
+		'Brush Ninja',
+		'brush.ninja',
+		'Create Animated Gifs in your Browser',
+	),
+	array(
+		'WP Toolbelt Plugin',
+		'wordpress.org/plugins/wp-toolbelt/',
+		'A privacy focused replacement for Jetpack',
+	),
+	array(
+		'Remove Background',
+		'www.remove.bg',
+		'Remove Backgrounds from Images',
+	),
+	array(
+		'UI Faces',
+		'uifaces.co',
+		'User Interface Faces',
+	),
+	array(
+		'Call to Idea',
+		'calltoidea.com',
+		'Interface Inspiration',
+	),
+	array(
+		'Codeable',
+		'codeable.io/?ref=OJHe0',
+		'Fix all your WordPress problems.',
+	),
+	array(
+		'CSS Reference',
+		'cssreference.io',
+		'CSS Reference',
+	),
+	array(
+		'Screely',
+		'www.screely.com',
+		'Make screenshots look nice quickly.',
+	),
+	array(
+		'Ethical.net',
+		'ethical.net',
+		'Ethical apps and services',
+	),
+	array(
+		'UK Postcode',
+		'postcodes.io',
+		'UK Postcodes API',
+	),
+	array(
+		'Mockup Photos',
+		'mockup.photos',
+		'Insert screenshots into different computing situations.',
+	),
+	array(
+		'Map Glyphs',
+		'mapglyphs.com/world-maps',
+		'World Map images.',
+	),
+	array(
+		'Hemingway App',
+		'hemingwayapp.com',
+		'An online text proof reader.',
+	),
+	array(
+		'Devdocs',
+		'devdocs.io',
+		'API docs for everything web.',
+	),
+	array(
+		'Brand Colors',
+		'brandcolors.net',
+		'Website brand colours, all in one place.',
+	),
+	array(
+		'Audio Mass',
+		'audiomass.co',
+		'Browser based audio editor (like Audacity).',
+	),
+	array(
+		'Lambda Test',
+		'www.lambdatest.com',
+		'An online browser testing tool.',
+	),
+	array(
+		'Clippy',
+		'bennettfeely.com/clippy',
+		'A CSS clipping path generator.',
+	),
+	array(
+		'Subtle Patterns',
+		'subtlepatterns.com',
+		'A collection of images that are great for website/ presentation backgrounds.',
+	),
+	array(
+		'SVG Porn (SFW)',
+		'svgporn.com',
+		'A huge collection of SVG logos and icons for various companies.',
+	),
+	array(
+		'HTAccess Tester',
+		'htaccess.madewithlove.be',
+		'HTAccess Tester.',
+	),
+
 );
 
 ?>
@@ -41,7 +129,12 @@ $resources = array(
 
 <div class="wrapper block-wrapper">
 <?php
-	foreach ( $resources as $url => $description ) {
+	foreach ( $resources as $resource ) {
+
+		$name = $resource[0];
+		$url = $resource[1];
+		$description = $resource[2];
+
 		if ( $url ) {
 			$image_url = site_screenshot( $url );
 ?>
@@ -50,7 +143,7 @@ $resources = array(
 			<img src="<?php echo $image_url; ?>" width="400" height="300" alt="<?php echo $url; ?> Screenshot" />
 		</a>
 		<div class="meta">
-			<h3><?php echo $url; ?></h3>
+			<h3><?php echo $name; ?></h3>
 			<p><?php echo $description; ?></p>
 		</div>
 	</div>
