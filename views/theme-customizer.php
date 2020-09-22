82f14587b22e5f632641e3ffa5e01d2683f7322f
Ben Gillbanks <binarymoon@gmail.com>
2020-09-22 21:44:35 +0100

    Update theme-customizer.php

--- views/theme-customizer.php
+++ views/theme-customizer.php
@@ -7,7 +7,7 @@
 
         <div class="wrapper text">
             <h1>Theme Customization</h1>
-            <p class="intro"><strong>Need some customisation to your theme?</strong> Our partners at <a href="https://api.referoo.co/s/mqFlk" target="_blank" rel="noopener">Codeable</a> are ready to take your job and professionally customise your theme as needed.</p>
+            <p class="intro"><strong>Need some customisation to your theme?</strong> Our partners at <a href="https://codeable.io/?ref=99Tpl" target="_blank" rel="noopener">Codeable</a> are ready to take your job and professionally customise your theme as needed.</p>
             <p>If there is a bug with the theme or some other issue then you can <a href="<?php echo path( '/contact/' ); ?>">contact us</a>. You can also find out the <a href="<?php echo path( 'documentation/general/support-vs-customization/'); ?>">difference between support and customization</a>.</p>
             <?php include( '_partials/codeable.php' ); ?>
         </div>
