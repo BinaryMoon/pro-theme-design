6e7ddaf008bc0e017088affee3a053125f584a89
Ben Gillbanks <binarymoon@gmail.com>
2021-08-25 10:54:15 +0100

    Make content more modular

---
+++ views/_partials/about-us.php
@@ -0,0 +1,57 @@
+<div class="wrapper map">
+
+<section class="origin">
+	<h4 class="minor">About Us</h4>
+	<h4 id="about-lead">Pro Theme Design began in 2007 as a collaboration between two web designers</h4>
+</section>
+
+<div class="blurb blurb-ben">
+
+	<img id="ava-ben" class="avatar" src="<?php echo image_path( 'website/ava-ben.jpg' ); ?>" alt="">
+	<h5>Ben Gillbanks</h5>
+	<p class="location">Devon, England</p>
+	<p class="bio">Ben is a full stack developer - doing both design and development - often turning Darrens ideas into real products, but not averse to designing his own themes and writing the occassional plugin. Plus he blogs about WordPress and tech at <a href="https://www.binarymoon.co.uk/">Binary Moon</a> and runs <a href="https://masterwp.co">MasterWP - WordPress newsletter</a>.</p>
+
+	<ul>
+		<li>
+			<?php draw_svg( 'media-link', 'small' ); ?>
+			<a href="https://www.binarymoon.co.uk" target="_blank" class="link" rel="noopener noreferrer">
+				Personal Site
+			</a>
+		</li>
+		<li>
+			<?php draw_svg( 'logo-social-twitter', 'small' ); ?>
+			<a href="https://twitter.com/binarymoon" target="_blank" class="link" rel="noopener noreferrer">
+				Follow Me on Twitter
+			</a>
+		</li>
+	</ul>
+
+</div>
+
+<div class="blurb blurb-darren">
+
+	<img id="ava-darren" class="pos avatar" src="<?php echo image_path( 'website/ava-darren.jpg' ); ?>" alt="">
+	<h5>Darren Hoyt</h5>
+	<p class="location">Brooklyn, NY</p>
+	<p class="bio">Established in the WordPress community for projects like <strong>Mimbo</strong> and <strong>Agregado</strong>, Darren also has 14 years experience designing websites for businesses and startups. His role at Pro Theme Design is taking what Ben builds and making it beautiful and simple to use.</p>
+
+	<ul>
+		<!-- <li>
+			<?php draw_svg( 'media-link', 'small' ); ?>
+			<a href="http://www.darrenhoyt.com" target="_blank" class="link" rel="noopener noreferrer">
+				Personal Site
+			</a>
+		</li> -->
+		<li>
+			<?php draw_svg( 'logo-social-twitter', 'small' ); ?>
+			<a href="https://www.twitter.com/darrenhoyt" target="_blank" class="link" rel="noopener noreferrer">
+				Follow Me on Twitter
+			</a>
+		</li>
+	</ul>
+
+
+</div>
+
+</div>
