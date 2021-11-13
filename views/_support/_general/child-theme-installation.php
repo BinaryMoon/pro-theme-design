86928f03c374483e89166d0f924edaae5d04181c
Ben Gillbanks <binarymoon@gmail.com>
2021-11-13 11:21:20 +0000

    Update child-theme-installation.php

--- views/_support/_general/child-theme-installation.php
+++ views/_support/_general/child-theme-installation.php
@@ -2,17 +2,23 @@
 
 <p class="note">Please note that on Mac OSX zip files get extracted by default - so check your downloads folder for the zip file before continuing.</p>
 
-<p>Child themes are different to regular WordPress themes. They rely on a 'parent' theme to function. If the parent theme is not installed then the child theme will not work.</p>
+<p>Child themes are different to regular WordPress themes. They rely on a <em>'parent'</em> theme to function, in this case that theme is Broadsheet. If the parent theme is not installed then the child theme will not work.</p>
+
+<p>When you buy Chronicle you will also get access to Broadsheet, there is no need to buy both themes separately.</p>
 
 <h2>Preparing Your Themes</h2>
 
-<p>After downloading the .zip file to your computer you will need to extract it. Inside are two more zip files - one for the parent theme, and one for the child theme. These will need to be installed individually. The order that you install them does not matter - however the child theme can not be activated until both themes have been installed.</p>
+<p>After downloading the .zip file to your computer you will need to extract it. Inside are two more zip files - one for the parent theme, and one for the child theme. These will need to be installed individually. The order that you install them does not matter - however Chronicle can not be activated until both themes have been installed.</p>
 
 <h2>Installation via the WordPress admin panel</h2>
 
-<p>Please go to Appearance → Themes in your WordPress admin panel and click on Add New/ Upload Theme. Now you an choose to upload the .zip file and click the Install now button. You will need to do the same for both themes.</p>
+<p>This will work for self hosted sites and WordPress.com business plan. Installing your own theme is not supported on wordpress.com plans below the business one.</p>
 
-<p>Once both themes are uploaded you can go to Appearance &rarr; Themes, and select the theme you want to activate from the list.</p>
+<ol>
+	<li>Go to <em>Appearance → Themes</em> in your WordPress admin panel and click on <em>Add New/ Upload Theme</em>.</li>
+	<li>Now you can upload the .zip file and click the <em>Install now</em> button. You will need to do the same for both themes.</li>
+	<li>Once both themes are added go to <em>Appearance &rarr; Themes</em>, and select the theme you want to activate from the list. Then press <em>activate</em></li>
+</ol>
 
 <h2>Installation via FTP</h2>
 
@@ -21,6 +27,6 @@
 <p>Once installed the themes should use the following directory structure.</p>
 
 <ul>
-    <li>wp-content/themes/broadsheet/..</li>
-    <li>wp-content/themes/chronicle/..</li>
+	<li><code>wp-content/themes/broadsheet/...</code></li>
+	<li><code>wp-content/themes/chronicle/...</code></li>
 </ul>
